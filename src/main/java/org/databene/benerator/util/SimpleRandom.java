271b6aaaf6af2d75fa63b8f4b5926dbc4fae0e09
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2008-03-27 15:29:34 +0000

    Moved from org.databene.benerator to here
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@477 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/main/java/org/databene/benerator/util/SimpleRandom.java
@@ -0,0 +1,74 @@
+/*
+ * (c) Copyright 2006 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.util;
+
+import java.util.Random;
+
+/**
+ * Provides utility functions for generating numbers in an interval.<br/>
+ * <br/>
+ * Created: 03.09.2006 13:23:02
+ */
+public class SimpleRandom {
+
+    /** The basic random provider */
+    private static Random random = new Random();
+
+    /** Generates a random long value in the range from min to max */
+    public static long randomLong(long min, long max) {
+        if (min > max)
+            throw new IllegalArgumentException("min > max: " + min + " > " + max);
+        long range = max - min + 1;
+        long result;
+        if (range != 0)
+            result = min + (random.nextLong() % range);
+        else
+            result = random.nextLong();
+        if (result < min)
+            result += range;
+        return result;
+    }
+
+    /** Generates a random int value in the range from min to max */
+    public static int randomInt(int min, int max) {
+        if (min > max)
+            throw new IllegalArgumentException("min > max: " + min + " > " + max);
+        int range = max - min + 1;
+        int result;
+        if (range != 0)
+            result = min + (random.nextInt() % range);
+        else
+            result = random.nextInt();
+        if (result < min)
+            result += range;
+        return result;
+    }
+
+    public static <T> T randomValue(T ... values) {
+        return values[randomInt(0, values.length)];
+    }
+}
