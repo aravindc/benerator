3a6d74439dbdc4260bacc345c4da777bfd8831d3
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2008-03-27 15:34:32 +0000

    Initial version
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@480 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/main/java/org/databene/benerator/primitive/datetime/DateTimeGenerator.java
@@ -0,0 +1,121 @@
+/*
+ * (c) Copyright 2008 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.primitive.datetime;
+
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+import org.databene.benerator.LightweightGenerator;
+import org.databene.benerator.primitive.number.adapter.LongGenerator;
+import org.databene.commons.TimeUtil;
+import org.databene.model.Sequence;
+import org.databene.model.String2DistributionConverter;
+
+/**
+ * Creates DateTimes with separate date and time distribution characteristics.<br/><br/>
+ * Created: 29.02.2008 18:19:55
+ * @since 0.5.0
+ * @author Volker Bergmann
+ */
+public class DateTimeGenerator extends LightweightGenerator<Date> {
+    
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    private LongGenerator dateGenerator = new LongGenerator();
+    private LongGenerator timeGenerator = new LongGenerator();
+    
+    private TimeZone timeZone = TimeZone.getDefault();
+    
+    public DateTimeGenerator() {
+        this(
+            TimeUtil.add(TimeUtil.today().getTime(), Calendar.YEAR, -1), 
+            TimeUtil.today().getTime(), 
+            TimeUtil.time(9, 0), 
+            TimeUtil.time(17, 0));
+    }
+
+    public DateTimeGenerator(Date minDate, Date maxDate, Time minTime, Time maxTine) {
+        setMinDate(minDate);
+        setMaxDate(maxDate);
+        setMinTime(minTime);
+        setMaxTime(maxTine);
+    }
+
+    // properties ------------------------------------------------------------------------------------------------------
+
+    public void setMinDate(Date minDate) {
+        dateGenerator.setMin(minDate.getTime());
+    }
+    
+    public void setMaxDate(Date maxDate) {
+        dateGenerator.setMax(maxDate.getTime());
+    }
+    
+    public void setDatePrecision(String datePrecision) {
+        dateGenerator.setPrecision(parseDate(datePrecision) + timeZone.getRawOffset());
+    }
+    
+    public void setDateDistribution(String distribution) {
+        dateGenerator.setDistribution(Sequence.getInstance(distribution, true));
+    }
+    
+    public void setMinTime(Time minTime) {
+        timeGenerator.setMin(minTime.getTime());
+    }
+    
+    public void setMaxTime(Time maxTime) {
+        timeGenerator.setMax(maxTime.getTime());
+    }
+    
+    public void setTimePrecision(Time timePrecision) {
+        timeGenerator.setPrecision(timePrecision.getTime());
+    }
+    
+    public void setTimeDistribution(String distribution) {
+        timeGenerator.setDistribution(String2DistributionConverter.parse(distribution));
+    }
+
+    // Generator interface ---------------------------------------------------------------------------------------------
+    
+    public Date generate() {
+        return new Date(dateGenerator.generate() + timeGenerator.generate());
+    }
+    
+    // private helper methods ------------------------------------------------------------------------------------------
+    
+    private long parseDate(String s) {
+        try {
+            return dateFormat.parse(s).getTime();
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
