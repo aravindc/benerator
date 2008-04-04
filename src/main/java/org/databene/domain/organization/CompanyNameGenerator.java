422690ec2510e3e6357fbfeb757eecced8b808e1
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2008-04-04 07:05:06 +0000

    Improved name generation by person names, sectors and legal forms
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@628 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/domain/organization/CompanyNameGenerator.java
+++ src/main/java/org/databene/domain/organization/CompanyNameGenerator.java
@@ -27,8 +27,18 @@
 package org.databene.domain.organization;
 
 import org.databene.benerator.Generator;
+import org.databene.benerator.csv.DatasetCSVGenerator;
+import org.databene.benerator.sample.ConstantGenerator;
 import org.databene.benerator.sample.SequencedCSVSampleGenerator;
 import org.databene.benerator.util.LightweightGenerator;
+import org.databene.benerator.wrapper.AlternativeGenerator;
+import org.databene.benerator.wrapper.ConvertingGenerator;
+import org.databene.benerator.wrapper.MessageGenerator;
+import org.databene.benerator.wrapper.NullableGenerator;
+import org.databene.commons.bean.PropertyAccessConverter;
+import org.databene.domain.address.City;
+import org.databene.domain.address.CityGenerator;
+import org.databene.domain.address.Country;
 
 /**
  * Generates company names.<br/><br/>
@@ -38,24 +48,63 @@ import org.databene.benerator.util.LightweightGenerator;
  */
 public class CompanyNameGenerator extends LightweightGenerator<String> {
 
-    private static Generator<String> gen1;
-    private static Generator<String> gen2;
+    private static final String ORG = "org/databene/domain/organization/";
+    private static final String PERS = "org/databene/domain/person/";
+    private static final String REGION  = "org/databene/dataset/region";
+    
+    private Generator<String> core;
+    private Generator<String> sector;
+    private Generator<String> legalForm;
+    private String dataset;
+    Generator<String> location;
     
     public CompanyNameGenerator() {
-        this("artificial");
     }
     
-    public CompanyNameGenerator(String style) {
-        setStyle(style);
-    }
-    
-    public void setStyle(String style) {
-        gen1 = new SequencedCSVSampleGenerator<String>("org/databene/domain/organization/" + style + "1.csv");
-        gen2 = new SequencedCSVSampleGenerator<String>("org/databene/domain/organization/" + style + "2.csv");
+    public CompanyNameGenerator(String dataset) {
+        this.dataset = dataset;
+        Generator<String> person = new MessageGenerator("{0} {1}", 
+                new DatasetCSVGenerator<String>(PERS + "givenName_male_{0}.csv", dataset, REGION),
+                new DatasetCSVGenerator<String>(PERS + "familyName_{0}.csv", dataset, REGION)
+            );
+        Generator<String> artificial = new MessageGenerator("{0}{1}", 
+                new SequencedCSVSampleGenerator<String>(ORG + "artificial1.csv"),
+                new SequencedCSVSampleGenerator<String>(ORG + "artificial2.csv")
+            );
+        Generator<String> tech = new MessageGenerator("{0}{1}", 
+                new SequencedCSVSampleGenerator<String>(ORG + "tech1.csv"),
+                new SequencedCSVSampleGenerator<String>(ORG + "tech2.csv")
+            );
+        Country country = Country.getInstance(dataset);
+        if (country != null) {
+            Generator<String> city = new ConvertingGenerator<City, String>(new CityGenerator(country), new PropertyAccessConverter("name"));
+            location = new NullableGenerator<String>(
+                    new AlternativeGenerator<String>(String.class,
+                            new ConstantGenerator<String>(country.getName()), city), 
+                    0.8
+                );
+            
+        } else
+            location = new ConstantGenerator<String>(null);
+        core = new AlternativeGenerator<String>(String.class, artificial, tech, person);
+        legalForm = new DatasetCSVGenerator<String>(ORG + "legalForm_{0}.csv", dataset, REGION);
+        sector = new NullableGenerator<String>(new DatasetCSVGenerator<String>(ORG + "sector_{0}.csv", dataset, REGION), 0.7);
     }
     
     public String generate() {
-        return gen1.generate() + gen2.generate();
+        StringBuilder builder = new StringBuilder(core.generate());
+        String sec = sector.generate();
+        if (sec != null)
+            builder.append(' ').append(sec);
+        String loc = location.generate();
+        if (loc != null)
+            builder.append(' ').append(loc);
+        builder.append(' ').append(legalForm.generate());
+        return builder.toString();
     }
 
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + '[' + dataset + ']';
+    }
 }
