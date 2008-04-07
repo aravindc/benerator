35c3f1c7c44ac4e12b250cd9e6b577a3323c81a2
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2008-04-07 10:33:53 +0000

    Initial version
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@636 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/test/java/org/databene/platform/csv/CSVEntityIteratorTest.java
@@ -0,0 +1,65 @@
+/*
+ * (c) Copyright 2008 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.platform.csv;
+
+import junit.framework.TestCase;
+import org.databene.model.data.Entity;
+import org.databene.model.data.ComplexTypeDescriptor;
+
+import java.util.Iterator;
+
+/**
+ * Tests the CSVEntityIterator.<br/>
+ * <br/>
+ * Created: 07.04.2008 12:30:17
+ * @since 0.5.1
+ * @author Volker Bergmann
+ */
+public class CSVEntityIteratorTest extends TestCase {
+
+    private static final String URI = "org/databene/platform/csv/person-bean.csv";
+
+    // test methods ----------------------------------------------------------------------------------------------------
+
+    public void test() {
+        CSVEntityIterable iterable = new CSVEntityIterable(URI, "Person", ',');
+        checkIteration(iterable.iterator());
+    }
+
+    // private helpers -------------------------------------------------------------------------------------------------
+
+    private void checkIteration(Iterator<Entity> iterator) {
+        ComplexTypeDescriptor descriptor = new ComplexTypeDescriptor("Person");
+        assertTrue(iterator.hasNext());
+        assertEquals(new Entity(descriptor, "name", "Alice", "age", "23"), iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(new Entity(descriptor, "name", "Bob", "age", "34"), iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(new Entity(descriptor, "name", "Charly", "age", "45"), iterator.next());
+        assertFalse(iterator.hasNext());
+    }
+}
