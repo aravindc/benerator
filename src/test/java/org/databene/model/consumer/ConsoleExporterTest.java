8d7287968ded8c37960d22c25cc7926fe1986281
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2008-04-28 17:52:18 +0000

    Initial version
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@819 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/test/java/org/databene/model/consumer/ConsoleExporterTest.java
@@ -0,0 +1,70 @@
+/*
+ * (c) Copyright 2007 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.model.consumer;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests the {@link ConsoleExporter}.<br/><br/>
+ * Created at 11.04.2008 06:58:53
+ * @since 0.5.2
+ * @author Volker Bergmann
+ */
+public class ConsoleExporterTest extends TestCase {
+	
+	public void testSimpleTypes() {
+		ConsoleExporter<Object> exporter = new ConsoleExporter<Object>();
+		try {
+			exporter.startConsuming("Test");
+			exporter.finishConsuming("Test");
+
+			exporter.startConsuming(1);
+			exporter.finishConsuming(1);
+			
+			exporter.startConsuming(1.);
+			exporter.finishConsuming(1.);
+			
+			exporter.startConsuming(true);
+			exporter.finishConsuming(true);
+			
+			Date date = new Date();
+			exporter.startConsuming(date);
+			exporter.finishConsuming(date);
+			
+			Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+			exporter.startConsuming(timestamp);
+			exporter.finishConsuming(timestamp);
+			
+		} finally {
+			exporter.flush();
+			exporter.close();
+		}
+	}
+}
