c5c8514fe042802e0c3a50cf3b325df11ed1d61d
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2008-05-18 11:03:35 +0000

    Initial version
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@1031 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/main/java/org/databene/benerator/composite/SimpleTypeEntityGenerator.java
@@ -0,0 +1,83 @@
+/*
+ * (c) Copyright 2008 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.composite;
+
+import org.databene.benerator.Generator;
+import org.databene.benerator.factory.GenerationSetup;
+import org.databene.benerator.factory.SimpleTypeGeneratorFactory;
+import org.databene.benerator.wrapper.GeneratorWrapper;
+import org.databene.commons.Assert;
+import org.databene.commons.Context;
+import org.databene.model.data.ComplexTypeDescriptor;
+import org.databene.model.data.Entity;
+import org.databene.model.data.SimpleTypeDescriptor;
+import org.databene.model.data.TypeDescriptor;
+
+/**
+ * Generates Entities that wrap a content of simple type.<br/><br/>
+ * Created at 11.05.2008 23:37:42
+ * @since 0.5.4
+ * @author Volker Bergmann
+ */
+public class SimpleTypeEntityGenerator extends GeneratorWrapper<Object, Entity> {
+	
+	private ComplexTypeDescriptor complexType;
+	
+	public SimpleTypeEntityGenerator(ComplexTypeDescriptor complexType, boolean unique, Context context, GenerationSetup setup) {
+		this(createSource(complexType, unique, context, setup), complexType);
+	}
+	
+	public SimpleTypeEntityGenerator(Generator source, ComplexTypeDescriptor complexType) {
+		super(source);
+		this.complexType = complexType;
+	}
+
+	// Generator interface implementation ------------------------------------------------------------------------------
+
+	public Entity generate() {
+		Object content = source.generate();
+		Entity entity = new Entity(complexType);
+		entity.setComponent(ComplexTypeDescriptor.__SIMPLE_CONTENT, content);
+		return entity;
+	}
+
+	public Class<Entity> getGeneratedType() {
+		return Entity.class;
+	}
+	
+	// private helpers -------------------------------------------------------------------------------------------------
+
+	private static Generator<Object> createSource(ComplexTypeDescriptor complexType, boolean unique, Context context, GenerationSetup setup) {
+		Assert.notNull(complexType, "complexType");
+		TypeDescriptor contentType = complexType.getComponent(ComplexTypeDescriptor.__SIMPLE_CONTENT).getType();
+		if (contentType instanceof SimpleTypeDescriptor)
+			return (Generator<Object>) SimpleTypeGeneratorFactory.create((SimpleTypeDescriptor) contentType, unique, context, setup);
+		else
+			throw new UnsupportedOperationException("Expected simple type as content, found: " + contentType.getName());
+	}
+
+}
