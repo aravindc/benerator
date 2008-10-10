bad1c50c9e878afbd16ea5f867c057a0aba62f54
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2008-10-10 09:55:07 +0000

    improved test
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@1331 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/test/java/org/databene/benerator/factory/ComponentBuilderFactoryTest.java
+++ src/test/java/org/databene/benerator/factory/ComponentBuilderFactoryTest.java
@@ -154,7 +154,8 @@ public class ComponentBuilderFactoryTest extends GeneratorTest {
 		IdDescriptor id = new IdDescriptor(componentName);
 		ComponentBuilder builder = createComponentBuilder(id);
 		Generator<Long> helper = new ComponentBuilderGenerator(builder, componentName);
-		expectGeneratedSequenceOnce(helper, 1L, 2L, 3L, 4L);
+		expectGeneratedSequenceOnce(helper, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L);
+		assertTrue(helper.available());
 	}
 	
 	/**
