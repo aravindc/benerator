301ed059bf0b1d0adfd2e2e2711acbfdd91abf88
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2008-11-27 07:40:19 +0000

    todo comment
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@1467 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/benerator/primitive/datetime/CurrentTimeGenerator.java
+++ src/main/java/org/databene/benerator/primitive/datetime/CurrentTimeGenerator.java
@@ -35,7 +35,7 @@ import java.util.Date;
  * <br/>
  * Created: 17.11.2007 13:07:18
  */
-public class CurrentTimeGenerator extends LightweightGenerator<Date> {
+public class CurrentTimeGenerator extends LightweightGenerator<Date> { // TODO this should return only time, not the date!
 
     public Date generate() {
         return new Date();
