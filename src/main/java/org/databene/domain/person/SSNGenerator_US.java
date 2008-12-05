56b45f209ede1fce080af23d8e02fbe68bd38940
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2008-12-05 14:18:37 +0000

    descoped tasks
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@1542 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/domain/person/SSNGenerator_US.java
+++ src/main/java/org/databene/domain/person/SSNGenerator_US.java
@@ -46,7 +46,7 @@ import org.databene.commons.StringUtil;
 
 public class SSNGenerator_US extends LightweightGenerator<String> {
 
-	// TODO support 'unique' property
+	// TODO v0.5.7 support 'unique' property
 	
 	private IntegerGenerator areaNumberGenerator;
 	private Generator<Integer> groupNumberGenerator;
