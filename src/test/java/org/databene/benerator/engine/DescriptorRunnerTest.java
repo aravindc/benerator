bc6a92e7dd4a0d8e7c7ff958f1e9cbec150c193c
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-03-15 13:31:32 +0000

    Initial version
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@2118 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/test/java/org/databene/benerator/engine/DescriptorRunnerTest.java
@@ -0,0 +1,68 @@
+/*
+ * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.engine;
+
+import java.util.List;
+
+import org.databene.model.consumer.FileExporter;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests the {@link DescriptorRunner}.<br/>
+ * <br/>
+ * Created at 13.03.2009 07:16:55
+ * @since 0.5.8
+ * @author Volker Bergmann
+ */
+
+public class DescriptorRunnerTest extends TestCase {
+
+    private static final String EXPORT_FILE_URI = "test-uri.txt";
+
+	public void testGetGeneratedFiles() {
+		DescriptorRunner runner = new DescriptorRunner("string://<setup/>");
+		runner.addResource(new TestExporter());
+		List<String> generatedFiles = runner.getGeneratedFiles();
+		assertEquals(1, generatedFiles.size());
+		assertEquals(EXPORT_FILE_URI, generatedFiles.get(0));
+	}
+	
+	
+	
+	static class TestExporter implements FileExporter<String> {
+
+        public String getUri() {
+	        return EXPORT_FILE_URI;
+        }
+
+        public void startConsuming(String object) { }
+        public void finishConsuming(String object) { }
+        public void flush() { }
+        public void close() { }
+	}
+}
