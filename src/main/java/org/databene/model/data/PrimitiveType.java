402ae0d711386c59e145273ddf8e63b5c0e89b75
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-03-15 12:51:35 +0000

    Removed generic type
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@2060 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/model/data/PrimitiveType.java
+++ src/main/java/org/databene/model/data/PrimitiveType.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2008 by Volker Bergmann. All rights reserved.
+ * (c) Copyright 2008-2009 by Volker Bergmann. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, is permitted under the terms of the
@@ -42,30 +42,30 @@ import java.util.Map;
  * @since 0.5.0
  * @author Volker Bergmann
  */
-public class PrimitiveType<E> {
+public class PrimitiveType {
     
-    private static final Map<String, PrimitiveType<? extends Object>> instances = new HashMap<String, PrimitiveType<? extends Object>>();
+    private static final Map<String, PrimitiveType> instances = new HashMap<String, PrimitiveType>();
     
-    public static final PrimitiveType<Byte>       BYTE        = new PrimitiveType<Byte>("byte", Byte.class);
-    public static final PrimitiveType<Short>      SHORT       = new PrimitiveType<Short>("short", Short.class);
-    public static final PrimitiveType<Integer>    INT         = new PrimitiveType<Integer>("int", Integer.class);
-    public static final PrimitiveType<Long>       LONG        = new PrimitiveType<Long>("long", Long.class);
-    public static final PrimitiveType<BigInteger> BIG_INTEGER = new PrimitiveType<BigInteger>("big_integer", BigInteger.class);
-    public static final PrimitiveType<Float>      FLOAT       = new PrimitiveType<Float>("float", Float.class);
-    public static final PrimitiveType<Double>     DOUBLE      = new PrimitiveType<Double>("double", Double.class);
-    public static final PrimitiveType<BigDecimal> BIG_DECIMAL = new PrimitiveType<BigDecimal>("big_decimal", BigDecimal.class);
-    public static final PrimitiveType<Boolean>    BOOLEAN     = new PrimitiveType<Boolean>("boolean", Boolean.class);
-    public static final PrimitiveType<String>     STRING      = new PrimitiveType<String>("string", String.class);
-    public static final PrimitiveType<Date>       DATE        = new PrimitiveType<Date>("date", Date.class);
-    public static final PrimitiveType<Time>       TIME        = new PrimitiveType<Time>("time", Time.class);
-    public static final PrimitiveType<Timestamp>  TIMESTAMP   = new PrimitiveType<Timestamp>("timestamp", Timestamp.class);
-    public static final PrimitiveType<Object>     OBJECT      = new PrimitiveType<Object>("object", Object.class);
-    public static final PrimitiveType<byte[]>     BINARY      = new PrimitiveType<byte[]>("binary", byte[].class);
+    public static final PrimitiveType BYTE        = new PrimitiveType("byte", Byte.class);
+    public static final PrimitiveType SHORT       = new PrimitiveType("short", Short.class);
+    public static final PrimitiveType INT         = new PrimitiveType("int", Integer.class);
+    public static final PrimitiveType LONG        = new PrimitiveType("long", Long.class);
+    public static final PrimitiveType BIG_INTEGER = new PrimitiveType("big_integer", BigInteger.class);
+    public static final PrimitiveType FLOAT       = new PrimitiveType("float", Float.class);
+    public static final PrimitiveType DOUBLE      = new PrimitiveType("double", Double.class);
+    public static final PrimitiveType BIG_DECIMAL = new PrimitiveType("big_decimal", BigDecimal.class);
+    public static final PrimitiveType BOOLEAN     = new PrimitiveType("boolean", Boolean.class);
+    public static final PrimitiveType STRING      = new PrimitiveType("string", String.class);
+    public static final PrimitiveType DATE        = new PrimitiveType("date", Date.class);
+    public static final PrimitiveType TIME        = new PrimitiveType("time", Time.class);
+    public static final PrimitiveType TIMESTAMP   = new PrimitiveType("timestamp", Timestamp.class);
+    public static final PrimitiveType OBJECT      = new PrimitiveType("object", Object.class);
+    public static final PrimitiveType BINARY      = new PrimitiveType("binary", byte[].class);
     
     private String   name;
-    private Class<E> javaType;
+    private Class<?> javaType;
     
-    public PrimitiveType(String name, Class<E> javaType) {
+    public PrimitiveType(String name, Class<?> javaType) {
         if (name == null)
             throw new IllegalArgumentException("name is null");
         if (javaType == null)
@@ -79,15 +79,15 @@ public class PrimitiveType<E> {
         return name;
     }
     
-    public Class<E> getJavaType() {
+    public Class<?> getJavaType() {
         return javaType;
     }
 
-    public static <T> PrimitiveType<T> getInstance(String parentName) {
-        return (PrimitiveType<T>) instances.get(parentName);
+    public static PrimitiveType getInstance(String parentName) {
+        return instances.get(parentName);
     }
 
-    public static Collection<PrimitiveType<? extends Object>> getInstances() {
+    public static Collection<PrimitiveType> getInstances() {
         return instances.values();
     }
 
