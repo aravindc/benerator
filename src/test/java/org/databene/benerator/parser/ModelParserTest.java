cb3e7b9969a52e383f7cfd35063b47f077aae54f
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-03-15 13:31:04 +0000

    Initial version
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@2117 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/test/java/org/databene/benerator/parser/ModelParserTest.java
@@ -0,0 +1,71 @@
+/*
+ * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.parser;
+
+import org.databene.benerator.engine.BeneratorContext;
+import org.databene.commons.xml.XMLUtil;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests the {@link ModelParser}.<br/>
+ * <br/>
+ * Created at 12.03.2009 18:19:12
+ * @since 0.5.8
+ * @author Volker Bergmann
+ */
+
+public class ModelParserTest extends TestCase {
+
+	public void testParseBeanClass() throws Exception {
+		ModelParser parser = new ModelParser(new BeneratorContext("."));
+		String beanXML = "<bean id='id' class='" + TestBean.class.getName() + "' />";
+		Object bean = parser.parseBean(XMLUtil.parseStringAsElement(beanXML));
+		assertEquals(TestBean.class, bean.getClass());
+		assertEquals(1, ((TestBean) bean).n);
+	}
+	
+	public void testParseBeanSpec() throws Exception {
+		ModelParser parser = new ModelParser(new BeneratorContext("."));
+		String beanXML = "<bean id='id' spec='" + TestBean.class.getName() + "(2)' />";
+		Object bean = parser.parseBean(XMLUtil.parseStringAsElement(beanXML));
+		assertEquals(TestBean.class, bean.getClass());
+		assertEquals(2, ((TestBean) bean).n);
+	}
+	
+	public static class TestBean {
+		public int n;
+		
+		public TestBean() {
+			this(1);
+		}
+
+        public TestBean(int n) {
+        	this.n = n;
+        }
+	}
+}
