1c124254f3f9a5dc4a4fb517351228235ee24b30
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-03-15 13:29:21 +0000

    Added tests
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@2115 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/test/java/org/databene/domain/person/SalutationProviderTest.java
+++ src/test/java/org/databene/domain/person/SalutationProviderTest.java
@@ -1,3 +1,29 @@
+/*
+ * (c) Copyright 2006-2009 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.databene.domain.person;
 
 import junit.framework.TestCase;
@@ -10,10 +36,19 @@ import java.util.Locale;
  */
 public class SalutationProviderTest extends TestCase {
 
-    public void test() {
+    public void testWesternLocales() {
         check(Locale.GERMAN,  "Frau", "Herr");
-        check(Locale.FRENCH,  "Mme",  "Mr");
+        check(Locale.FRENCH,  "Mme",  "M.");
         check(Locale.ENGLISH, "Mrs.", "Mr.");
+    }
+    
+    public void testCyrillicLocales() {
+        check(new Locale("ru"), "Госпожа", "Господин");
+        check(new Locale("cs"), "Paní", "Pan");
+    }
+
+    public void testFallback() {
+        check(new Locale("xx"), "Mrs.", "Mr.");
     }
 
     private void check(Locale locale, String femaleSalutation, String maleSalutation) {
