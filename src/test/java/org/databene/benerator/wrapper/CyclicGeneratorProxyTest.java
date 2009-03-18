f092ccdb42b0ecf0796f85b8104df79b8ccd6da6
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-03-18 08:32:40 +0000

    Initial version
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@2170 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/test/java/org/databene/benerator/wrapper/CyclicGeneratorProxyTest.java
@@ -0,0 +1,108 @@
+/*
+ * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.wrapper;
+
+import junit.framework.TestCase;
+
+import org.databene.benerator.Generator;
+import org.databene.benerator.IllegalGeneratorStateException;
+import org.databene.benerator.InvalidGeneratorSetupException;
+
+/**
+ * Tests the {@link CyclicGeneratorProxy}.<br/>
+ * <br/>
+ * Created at 18.03.2009 09:17:10
+ * @since 0.5.9
+ * @author Volker Bergmann
+ */
+
+public class CyclicGeneratorProxyTest extends TestCase {
+	
+	public void testSingleIteration() {
+		CyclicGeneratorProxy<Integer> proxy = new CyclicGeneratorProxy<Integer>(new Source12());
+		expect12(proxy);
+		proxy.close();
+		assertFalse(proxy.available());
+	}
+
+	public void testCyclicIteration() {
+		CyclicGeneratorProxy<Integer> proxy = new CyclicGeneratorProxy<Integer>(new Source12());
+		expect12(proxy);
+		expect12(proxy);
+		proxy.close();
+		assertFalse(proxy.available());
+	}
+
+	public void testReset() {
+		CyclicGeneratorProxy<Integer> proxy = new CyclicGeneratorProxy<Integer>(new Source12());
+		assertTrue(proxy.available());
+		proxy.reset();
+		expect12(proxy);
+		proxy.close();
+		assertFalse(proxy.available());
+	}
+	
+	// helper methods --------------------------------------------------------------------------------------------------
+
+	private void expect12(CyclicGeneratorProxy<Integer> wrapper) {
+	    assertTrue(wrapper.available());
+		assertEquals(1, (int) wrapper.generate());
+		assertTrue(wrapper.available());
+		assertEquals(2, (int) wrapper.generate());
+    }
+
+	static class Source12 implements Generator<Integer> {
+		
+		private int n = 0;
+		
+        public Class<Integer> getGeneratedType() {
+	        return Integer.class;
+        }
+
+		public boolean available() {
+			return (n < 2);
+		}
+
+        public Integer generate() throws IllegalGeneratorStateException {
+	        n = (n < 3 ? n + 1 : 1);
+	        return n;
+        }
+
+        public void close() {
+	        n = 3;
+        }
+
+        public void reset() throws IllegalGeneratorStateException {
+	        n = 0;
+        }
+
+        public void validate() throws InvalidGeneratorSetupException {
+	        // always valid
+        }
+		
+	}
+}
