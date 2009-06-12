23975075ed90a84878be375a26a67b826f03bc49
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-06-12 09:12:31 +0000

    Bug fix 2805327 Time generation of DateTimeGenerator
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@2284 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/benerator/primitive/number/distribution/StepLongGenerator.java
+++ src/main/java/org/databene/benerator/primitive/number/distribution/StepLongGenerator.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2006 by Volker Bergmann. All rights reserved.
+ * (c) Copyright 2006-2009 by Volker Bergmann. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, is permitted under the terms of the
@@ -67,6 +67,16 @@ public class StepLongGenerator extends AbstractLongGenerator {
 
     public void setNext(long next) {
         this.next = next;
+    }
+    
+    @Override
+    public void setPrecision(Long precision) {
+    	super.setPrecision(precision);
+    	long absPrecision = Math.abs(precision);
+    	if (Math.abs(variation1) < absPrecision)
+    		variation1 = absPrecision;
+    	if (Math.abs(variation2) < absPrecision)
+    		variation2 = absPrecision;
     }
 
     // Generator implementation ----------------------------------------------------------------------------------------
