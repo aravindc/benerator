518be88dc8e9efdee62ba2c7b86e58f07a4d6559
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-06-12 09:10:28 +0000

    Bug fix 2805329 DateTimeGenerator doesn't stop at maxDate
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@2281 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/test/java/org/databene/benerator/primitive/datetime/DateTimeGeneratorTest.java
+++ src/test/java/org/databene/benerator/primitive/datetime/DateTimeGeneratorTest.java
@@ -10,25 +10,56 @@ import org.databene.commons.TimeUtil;
 import junit.framework.TestCase;
 
 public class DateTimeGeneratorTest extends TestCase {
+	
+	static final int N = 100;
 
     public void testInvalidSettings() {
         new DateTimeGenerator();
     }
     
-    public void testGeneration() {
+    public void testMinMax() {
         check(TimeUtil.date(1970, 0, 1), TimeUtil.date(1970, 0,  1), TimeUtil.time(12, 0), TimeUtil.time(12, 00));
         check(TimeUtil.date(1970, 0, 1), TimeUtil.date(1970, 0,  1), TimeUtil.time( 0, 0), TimeUtil.time(23, 59));
         check(TimeUtil.date(2008, 6, 5), TimeUtil.date(2008, 6, 25), TimeUtil.time( 9, 0), TimeUtil.time(17,  0));
     }
+
+    public void testDateDistribution() {
+    	int minYear = 2008;
+    	int maxYear = 2008;
+    	int hour = 1;
+    	int minute = 2;
+    	int second = 3;
+    	int millisecond = 4;
+    	
+        DateTimeGenerator generator = createGenerator(
+        		TimeUtil.date(minYear, 7, 6), 
+        		TimeUtil.date(maxYear, 8, 8),
+        		TimeUtil.time(hour, minute, second, millisecond), 
+        		TimeUtil.time(hour, minute, second, millisecond));
+        Date minDate = TimeUtil.date(minYear, 7, 6, hour, minute, second, millisecond);
+        Date maxDate = TimeUtil.date(maxYear, 8, 8, hour, minute, second, millisecond);
+        generator.setDatePrecision("0000-00-01");
+        generator.setDateDistribution("step");
+        assertTrue(generator.available());
+        for (int i = 0; i < N && generator.available(); i++) {
+            Date date = generator.generate();
+            assertFalse(date.before(minDate));
+            assertFalse(date.after(maxDate));
+            Calendar cal = new GregorianCalendar();
+            cal.setTime(date);
+            assertEquals(hour, cal.get(Calendar.HOUR));
+            assertEquals(minute, cal.get(Calendar.MINUTE));
+            assertEquals(second, cal.get(Calendar.SECOND));
+            assertEquals(millisecond, cal.get(Calendar.MILLISECOND));
+        }
+    }
+    
+    // private helpers ---------------------------------------------------------
     
     private void check(Date minDate, Date maxDate, Time minTime, Time maxTime) {
-        DateTimeGenerator generator = new DateTimeGenerator();
-        generator.setMinDate(minDate);
-        generator.setMaxDate(maxDate);
-        generator.setMinTime(minTime);
-        generator.setMaxTime(maxTime);
+        DateTimeGenerator generator = createGenerator(minDate, maxDate, minTime, maxTime);
         Date maxResult = new Date(maxDate.getTime() + maxTime.getTime());
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < N; i++) {
             Date date = generator.generate();
             assertFalse(date.before(minDate));
             assertFalse(date.after(maxResult));
@@ -36,4 +67,14 @@ public class DateTimeGeneratorTest extends TestCase {
             cal.setTime(date);
         }
     }
+
+	private DateTimeGenerator createGenerator(Date minDate, Date maxDate, Time minTime, Time maxTime) {
+	    DateTimeGenerator generator = new DateTimeGenerator();
+        generator.setMinDate(minDate);
+        generator.setMaxDate(maxDate);
+        generator.setMinTime(minTime);
+        generator.setMaxTime(maxTime);
+	    return generator;
+    }
+	
 }
