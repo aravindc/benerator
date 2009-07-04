f01ffcaa52f27d126b8128a977a6b946b79a95aa
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-07-04 10:08:58 +0000

    Initial version
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@2422 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/main/java/org/databene/benerator/distribution/AbstractWeightFunction.java
@@ -0,0 +1,70 @@
+/*
+ * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.distribution;
+
+import org.databene.benerator.Generator;
+import org.databene.benerator.primitive.number.adapter.WrapperFactory;
+import org.databene.benerator.sample.AttachedWeightSampleGenerator;
+import org.databene.benerator.util.GeneratorUtil;
+import org.databene.commons.BeanUtil;
+
+/**
+ * Abstract implementation of the {@link WeightFunction} interface.<br/>
+ * <br/>
+ * Created at 30.06.2009 07:13:49
+ * @since 0.6.0
+ * @author Volker Bergmann
+ */
+
+public abstract class AbstractWeightFunction implements WeightFunction {
+
+    @SuppressWarnings("unchecked")
+    public <S, P> Generator<P> applyTo(Generator<S> source) {
+	    return (Generator<P>) new AttachedWeightSampleGenerator<S>(source.getGeneratedType(), this, GeneratorUtil.allProducts(source));
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends Number> Generator<T> createGenerator(Class<T> numberType, T min, T max, T precision) {
+    	if (Long.class.equals(numberType))
+    		return (Generator<T>) createLongGenerator(min, max, precision);
+    	else if (Double.class.equals(numberType))
+    		return (Generator<T>) createDoubleGenerator(min, max, precision);
+    	else if (BeanUtil.isIntegralNumber(numberType.getName()))
+    		return WrapperFactory.wrapNumberGenerator(numberType, createLongGenerator(min, max, precision));
+    	else
+    		return WrapperFactory.wrapNumberGenerator(numberType, createDoubleGenerator(min, max, precision));
+    }
+
+	private <T extends Number> WeightedLongGenerator createLongGenerator(T min, T max, T precision) {
+	    return new WeightedLongGenerator(min.longValue(), max.longValue(), precision.longValue(), this);
+    }
+
+	private <T extends Number> WeightedDoubleGenerator createDoubleGenerator(T min, T max, T precision) {
+	    return new WeightedDoubleGenerator(min.doubleValue(), max.doubleValue(), precision.doubleValue(), this);
+    }
+
+}
