432f1b5bd8f1a99eba70ec5c49a67b4765dcd16d
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-07-24 21:15:09 +0000

    Disclaimer + Javadoc
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@2535 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/domain/person/FamilyNameGenerator.java
+++ src/main/java/org/databene/domain/person/FamilyNameGenerator.java
@@ -1,3 +1,29 @@
+/*
+ * (c) Copyright 2006-2009 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License.
+ *
+ * For redistributing this software or a derivative work under a license other
+ * than the GPL-compatible Free Software License as defined by the Free
+ * Software Foundation or approved by OSI, you must first obtain a commercial
+ * license to this software product from Volker Bergmann.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.databene.domain.person;
 
 import java.util.Locale;
@@ -5,8 +31,10 @@ import java.util.Locale;
 import org.databene.benerator.csv.WeightedDatasetCSVGenerator;
 
 /**
- * (c) Copyright 2006 by Volker Bergmann
+ * Generates family names.<br/>
+ * <br/>
  * Created: 09.06.2006 22:03:56
+ * @since 0.1
  * @author Volker Bergmann
  */
 public class FamilyNameGenerator extends WeightedDatasetCSVGenerator<String> {
@@ -24,4 +52,5 @@ public class FamilyNameGenerator extends WeightedDatasetCSVGenerator<String> {
     public FamilyNameGenerator(String datasetName, String nesting, String fileNamePattern) {
         super(fileNamePattern, datasetName, nesting, "UTF-8");
     }
+    
 }
