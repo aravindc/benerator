ba47d36f2d01bce6f3dbc2fdf9d0170a53f3334b
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-10-10 09:24:37 +0000

    added task
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@2804 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/benerator/Version.java
+++ src/main/java/org/databene/benerator/Version.java
@@ -36,7 +36,7 @@ package org.databene.benerator;
 
 public class Version {
 
-	public static final String VERSION = "0.6.0";
+	public static final String VERSION = "0.6.0"; // TODO generate this from the maven version settings
 
 	public static void main(String[] args) {
 		System.out.println(VERSION);
