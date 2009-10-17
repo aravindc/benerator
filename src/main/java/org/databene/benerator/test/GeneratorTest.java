ce8b0328fb9cb802419580a149f73bd7f8c70295
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-10-17 05:09:54 +0000

    Made logger protected
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@2930 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/benerator/test/GeneratorTest.java
+++ src/main/java/org/databene/benerator/test/GeneratorTest.java
@@ -53,7 +53,7 @@ import org.slf4j.LoggerFactory;
  */
 public abstract class GeneratorTest {
 
-    public final Logger logger = LoggerFactory.getLogger(getClass());
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
     
     protected BeneratorContext context;
 
