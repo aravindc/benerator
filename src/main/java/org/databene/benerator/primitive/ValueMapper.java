116e600cd70dc3f065bedf91c9d18be5bbb63bb3
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-10-24 15:25:46 +0000

    Initial version
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@3004 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/main/java/org/databene/benerator/primitive/ValueMapper.java
@@ -0,0 +1,93 @@
+/*
+ * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.primitive;
+
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.databene.benerator.script.BeneratorScriptParser;
+import org.databene.benerator.script.WeightedTransition;
+import org.databene.commons.ConversionException;
+import org.databene.commons.Converter;
+
+/**
+ * Converter implementation that maps input values in a 'Map' style.<br/><br/>
+ * Created: 24.10.2009 09:05:58
+ * @since 0.6.0
+ * @author Volker Bergmann
+ */
+public class ValueMapper implements Converter<Object, Object> { // TODO test in descriptor
+
+	private Map<Object, Object> mappings;
+	private boolean lenient;
+	
+	public ValueMapper() {
+		try {
+	        init(null, false);
+        } catch (ParseException e) {
+	        // this cannot happen
+        }
+	}
+
+	public ValueMapper(String mappingSpec) throws ParseException {
+		this(mappingSpec, false);
+	}
+
+	public ValueMapper(String mappingSpec, boolean lenient) throws ParseException {
+		init(mappingSpec, lenient);
+	}
+
+	private void init(String mappingSpec, boolean lenient) throws ParseException {
+	    this.mappings = new HashMap<Object, Object>();
+		setMappings(mappingSpec);
+		this.lenient = lenient;
+    }
+
+	public void setMappings(String mappingSpec) throws ParseException {
+		if (mappingSpec != null) {
+			WeightedTransition[] tl = BeneratorScriptParser.parseTransitionList(mappingSpec);
+			for (WeightedTransition t : tl)
+				mappings.put(t.getFrom(), t.getTo());
+		} else
+			mappings.clear();
+	}
+	
+	public Class<Object> getTargetType() {
+	    return Object.class; // TODO return common (super) type
+    }
+	
+	public boolean canConvert(Object sourceValue) {
+	    return (lenient || mappings.containsKey(sourceValue));
+    }
+
+	public Object convert(Object sourceValue) throws ConversionException {
+		if (!mappings.containsKey(sourceValue))
+			if (lenient)
+				return sourceValue;
+			else
+				throw new IllegalArgumentException("Cannot convert value: " + sourceValue);
+		else 
+			return mappings.get(sourceValue);
+    }
+
+}
