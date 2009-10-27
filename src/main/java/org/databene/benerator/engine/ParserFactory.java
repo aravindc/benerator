eace5124773105fd5f72a9f3374989fbb7511c84
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-10-27 06:15:51 +0000

    Redesigned Descriptor parsing
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@3038 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/main/java/org/databene/benerator/engine/ParserFactory.java
@@ -0,0 +1,71 @@
+/*
+ * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.engine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.databene.benerator.engine.parser.xml.BeanParser;
+import org.databene.benerator.engine.parser.xml.CommentParser;
+import org.databene.benerator.engine.parser.xml.CreateOrUpdateEntityParser;
+import org.databene.benerator.engine.parser.xml.DatabaseParser;
+import org.databene.benerator.engine.parser.xml.DefaultComponentParser;
+import org.databene.benerator.engine.parser.xml.EchoParser;
+import org.databene.benerator.engine.parser.xml.EvaluateParser;
+import org.databene.benerator.engine.parser.xml.ImportParser;
+import org.databene.benerator.engine.parser.xml.IncludeParser;
+import org.databene.benerator.engine.parser.xml.PropertyParser;
+import org.databene.benerator.engine.parser.xml.RunTaskParser;
+import org.databene.commons.ConfigurationError;
+
+/**
+ * TODO Document class.<br/><br/>
+ * Created: 25.10.2009 00:20:33
+ * @since 0.6.0
+ * @author Volker Bergmann
+ */
+public class ParserFactory {
+	
+	private static List<DescriptorParser> parsers = new ArrayList<DescriptorParser>();
+	
+	static { // TODO define extension mechanism (e.g. by PlatformDescriptor?)
+		parsers.add(new DefaultComponentParser());
+		parsers.add(new CommentParser());
+		parsers.add(new BeanParser());
+		parsers.add(new CreateOrUpdateEntityParser());
+		parsers.add(new DatabaseParser());
+		parsers.add(new EchoParser());
+		parsers.add(new EvaluateParser());
+		parsers.add(new ImportParser());
+		parsers.add(new IncludeParser());
+		parsers.add(new PropertyParser());
+		parsers.add(new RunTaskParser());
+	}
+
+	public static DescriptorParser getParser(String elementName, String parentName) {
+		for (DescriptorParser parser : parsers)
+			if (parser.supports(elementName, parentName))
+				return parser;
+		throw new ConfigurationError("Unknown element: " + elementName);
+    }
+
+}
