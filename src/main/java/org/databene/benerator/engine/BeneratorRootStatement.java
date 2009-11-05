317d7227ad799673a1ddcc930139aff2c734aa36
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-11-05 07:07:01 +0000

    renamed CreateOrUpdateEntityStatement to CreateOrUpdateEntitiesStatement
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@3164 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/benerator/engine/BeneratorRootStatement.java
+++ src/main/java/org/databene/benerator/engine/BeneratorRootStatement.java
@@ -23,7 +23,7 @@ package org.databene.benerator.engine;
 
 import org.databene.benerator.Generator;
 import org.databene.benerator.engine.statement.CompositeStatement;
-import org.databene.benerator.engine.statement.CreateOrUpdateEntityStatement;
+import org.databene.benerator.engine.statement.CreateOrUpdateEntitiesStatement;
 import org.databene.benerator.engine.statement.GenerateAndConsumeEntityTask;
 import org.databene.benerator.engine.statement.LazyStatement;
 import org.databene.benerator.engine.statement.StatementProxy;
@@ -67,8 +67,8 @@ public class BeneratorRootStatement extends CompositeStatement {
 		public void visit(Statement element) {
 			if (result != null)
 				return;
-			if (element instanceof CreateOrUpdateEntityStatement) {
-				CreateOrUpdateEntityStatement candidate = (CreateOrUpdateEntityStatement) element;
+			if (element instanceof CreateOrUpdateEntitiesStatement) {
+				CreateOrUpdateEntitiesStatement candidate = (CreateOrUpdateEntitiesStatement) element;
 				GenerateAndConsumeEntityTask target = candidate.getTarget();
 				if (name.equals(target.getTaskName())) {
 					result = target.getEntityGenerator();
