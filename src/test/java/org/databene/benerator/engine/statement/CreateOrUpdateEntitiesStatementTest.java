a9d62b35d1d8529401c0fb888bdc6aef8d226d5f
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-11-06 08:48:12 +0000

    Testing thread count and concurrency
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@3172 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/test/java/org/databene/benerator/engine/statement/CreateOrUpdateEntitiesStatementTest.java
@@ -0,0 +1,81 @@
+/*
+ * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.engine.statement;
+
+import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.databene.benerator.IllegalGeneratorStateException;
+import org.databene.benerator.engine.BeneratorContext;
+import org.databene.benerator.util.LightweightGenerator;
+import org.databene.commons.Expression;
+import org.databene.commons.expression.ConstantExpression;
+import org.databene.model.data.Entity;
+import org.junit.Test;
+
+/**
+ * Tests the {@link CreateOrUpdateEntitiesStatement}.<br/><br/>
+ * Created: 05.11.2009 08:18:17
+ * @since 0.6.0
+ * @author Volker Bergmann
+ */
+public class CreateOrUpdateEntitiesStatementTest {
+
+	private static final int THREAD_COUNT = 30;
+	private static final long INVOCATION_COUNT = 6000L;
+
+	@Test
+	public void testThreadCount() {
+		EntityGeneratorMock entityGenerator = new EntityGeneratorMock();
+		
+		GenerateAndConsumeEntityTask task = new GenerateAndConsumeEntityTask("myTask", entityGenerator, null, false, null);
+		
+		Expression<Long> count = new ConstantExpression<Long>(INVOCATION_COUNT);
+		Expression<Long> pageSize = new ConstantExpression<Long>(300L);
+		Expression<Integer> threads = new ConstantExpression<Integer>(THREAD_COUNT);
+		CreateOrUpdateEntitiesStatement statement = new CreateOrUpdateEntitiesStatement(task, count, pageSize, null, threads);
+		statement.execute(new BeneratorContext());
+		
+		assertEquals(INVOCATION_COUNT, entityGenerator.invocationCount);
+		assertEquals(THREAD_COUNT, entityGenerator.threads.size());
+	}
+	
+	class EntityGeneratorMock extends LightweightGenerator<Entity> {
+		
+		public int invocationCount;
+		public Set<Thread> threads = new HashSet<Thread>();
+
+		public Class<Entity> getGeneratedType() {
+	        return Entity.class;
+        }
+		
+		public Entity generate() throws IllegalGeneratorStateException {
+			int tmp = invocationCount;
+			threads.add(Thread.currentThread());
+            invocationCount = tmp + 1; // update is slightly delayed in order to provoke update errors 
+	        return null;               // in case of concurrency issues
+        }
+	}
+	
+}
