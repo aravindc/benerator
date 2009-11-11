e5b729ed6eef7f65ac73884b013fb113592c0a6a
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-11-11 16:47:33 +0000

    updated to new weight syntax
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@3196 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/test/java/org/databene/benerator/sample/StateGeneratorTest.java
+++ src/test/java/org/databene/benerator/sample/StateGeneratorTest.java
@@ -85,7 +85,7 @@ public class StateGeneratorTest extends GeneratorTest {
 	/** Tests a setup that generates Sequences (1, 2)*, e.g. (1, 2), (1, 2, 1, 2), (1, 2, 1, 2, 1, 2), ... */
 	@Test
 	public void testRandomSequenceByStringSpec() {
-		StateGenerator<String> generator = new StateGenerator<String>("null->1,1->2,2->1[0.5],2->null[0.5]");
+		StateGenerator<String> generator = new StateGenerator<String>("null->1,1->2,2->1^0.5,2->null^0.5");
 		for (int n = 0; n < 10; n++) {
 			List<String> products = GeneratorUtil.allProducts(generator);
 			assertTrue(products.size() % 2 == 0);
