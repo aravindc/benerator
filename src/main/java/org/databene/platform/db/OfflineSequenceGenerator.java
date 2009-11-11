8fa22b33ae729a217fe605038a5b8ef606f1ec88
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-11-11 18:12:24 +0000

    Introduced OfflineSequenceGenerator
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@3220 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/main/java/org/databene/platform/db/OfflineSequenceGenerator.java
@@ -0,0 +1,104 @@
+/*
+ * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.platform.db;
+
+import java.sql.SQLException;
+
+import org.databene.benerator.IllegalGeneratorStateException;
+import org.databene.benerator.InvalidGeneratorSetupException;
+import org.databene.benerator.util.LightweightGenerator;
+import org.databene.commons.StringUtil;
+
+/**
+ * Reads the current value of a sequence on first invocation, 
+ * increases the value locally on subsequent calls and 
+ * finally (on close()) updates the DB sequence with the local value.
+ * This saves database round trips but limits execution to a single 
+ * client.<br/><br/>
+ * Created: 11.11.2009 18:35:26
+ * @since 0.6.0
+ * @author Volker Bergmann
+ */
+public class OfflineSequenceGenerator extends LightweightGenerator<Long> {
+
+	private DBSystem target;
+	private String sequenceName;
+	private boolean initialized;
+	private long initial;
+	private long next;
+	
+	public OfflineSequenceGenerator() {
+		this(null, null);
+    }
+
+	public OfflineSequenceGenerator(DBSystem target, String sequenceName) {
+		this.target = target;
+	    this.sequenceName = sequenceName;
+	    this.initialized = false;
+    }
+	
+	public DBSystem getTarget() {
+    	return target;
+    }
+
+	public void setTarget(DBSystem target) {
+    	this.target = target;
+    }
+
+	public String getSequenceName() {
+    	return sequenceName;
+    }
+
+	public void setSequenceName(String sequenceName) {
+    	this.sequenceName = sequenceName;
+    }
+	
+	@Override
+	public void validate() {
+	    super.validate();
+	    if (target == null)
+	    	throw new InvalidGeneratorSetupException("No 'target' database defined");
+	    if (StringUtil.isEmpty(sequenceName))
+	    	throw new InvalidGeneratorSetupException("No sequence name defined");
+	}
+
+	public Class<Long> getGeneratedType() {
+	    return Long.class;
+    }
+	
+	public Long generate() throws IllegalGeneratorStateException {
+	    if (!initialized)
+	    	next = initial = target.nextSequenceValue(sequenceName);
+	    return next++;
+    }
+	
+	@Override
+	public void close() {
+	    try {
+	        super.close();
+	        target.incrementSequenceValue(sequenceName, next - initial);
+        } catch (SQLException e) {
+	        throw new RuntimeException(e);
+        }
+	}
+	
+}
---
+++ src/test/java/org/databene/platform/db/OfflinceSequenceGeneratorTest.java
@@ -0,0 +1,68 @@
+/*
+ * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.platform.db;
+
+import static org.junit.Assert.*;
+
+import org.databene.benerator.Generator;
+import org.databene.benerator.test.GeneratorClassTest;
+import org.databene.commons.db.hsql.HSQLUtil;
+import org.junit.Test;
+
+/**
+ * Tests the OfflineSequenceGenerator.<br/><br/>
+ * Created: 11.11.2009 18:50:58
+ * @since 0.6.0
+ * @author Volker Bergmann
+ */
+public class OfflinceSequenceGeneratorTest extends GeneratorClassTest {
+
+	public OfflinceSequenceGeneratorTest() {
+	    super(OfflineSequenceGenerator.class);
+    }
+
+	@Test
+	public void testLifeCycle() throws Exception {
+		DBSystem db = new DBSystem("db", 
+				HSQLUtil.IN_MEMORY_URL_PREFIX + "benerator", 
+				HSQLUtil.DRIVER, HSQLUtil.DEFAULT_USER, HSQLUtil.DEFAULT_PASSWORD);
+		String seq = getClass().getSimpleName();
+		try {
+			// create sequence and read its value
+			db.createSequence(seq);
+			long n = db.nextSequenceValue(seq);
+			// assure that the generated values are like if they stem from the DB sequence
+			Generator<Long> generator = new OfflineSequenceGenerator(db, seq);
+			generator.validate();
+			for (int i = 0; i < 10; i++) {
+				assertTrue(generator.available());
+				assertEquals(++n, generator.generate().longValue());
+			}
+			// assure that after closing the generator, the DB sequence continues as if it had been used itself
+			generator.close();
+			assertEquals(n + 1, db.nextSequenceValue(seq));
+		} finally {
+			db.dropSequence(seq);
+		}
+	}
+	
+}
