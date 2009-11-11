331f16df86a69d423df9b5add05b6db34a9946f5
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-11-11 16:51:18 +0000

    Resting nesting of elements in CreateOrUpdateEntitiesParser
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@3202 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/test/java/org/databene/benerator/engine/parser/xml/CreateOrUpdateEntitiesParserTest.java
@@ -0,0 +1,58 @@
+/*
+ * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.engine.parser.xml;
+
+import static org.junit.Assert.assertEquals;
+
+import org.databene.benerator.engine.BeneratorContext;
+import org.databene.benerator.engine.ResourceManagerSupport;
+import org.databene.benerator.engine.Statement;
+import org.databene.commons.xml.XMLUtil;
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+/**
+ * TODO Document class.<br/><br/>
+ * Created: 10.11.2009 15:08:46
+ * @since TODO version
+ * @author Volker Bergmann
+ */
+public class CreateOrUpdateEntitiesParserTest {
+
+	@Test
+	public void testBeanInvocation() throws Exception {
+        String uri = "string://<create-entities type='dummy' count='3'>" +
+        		"<execute>bean.invoke(2)</execute>" +
+        		"</create-entities>";
+		Document doc = XMLUtil.parse(uri);
+		CreateOrUpdateEntitiesParser parser = new CreateOrUpdateEntitiesParser();
+		Statement statement = parser.parse(doc.getDocumentElement(), new ResourceManagerSupport());
+		BeneratorContext context = new BeneratorContext();
+		BeanMock bean = new BeanMock();
+		bean.invocationCount = 0;
+		context.set("bean", bean);
+		statement.execute(context);
+		assertEquals(3, bean.invocationCount);
+		assertEquals(2, bean.lastValue);
+	}
+
+}
