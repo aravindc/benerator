513f2ba6e1fe78f96bb2343e7788d3ad992c57e5
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2009-12-10 06:06:18 +0000

    Tests the interaction between CreateOrUpdateEntitiesParser and CreateOrUpdateEntitiesStatement
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@3239 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/test/java/org/databene/benerator/engine/CreateOrUpdateEntitiesTest.java
@@ -0,0 +1,84 @@
+/*
+ * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.engine;
+
+import static org.junit.Assert.*;
+
+import org.databene.benerator.engine.parser.xml.CreateOrUpdateEntitiesParser;
+import org.databene.benerator.factory.ConsumerMock;
+import org.databene.commons.xml.XMLUtil;
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+/**
+ * Integration test for {@link CreateOrUpdateEntitiesParser} 
+ * and {@link CreateOrUpdateEntitiesTest}.<br/><br/>
+ * Created: 12.11.2009 16:05:53
+ * @since 0.6.0
+ * @author Volker Bergmann
+ */
+public class CreateOrUpdateEntitiesTest {
+
+	@Test
+	public void testAttributes() throws Exception {
+		String uri = "string://<create-entities type='dummy' count='{c}' threads='{tc}' pageSize='{ps}'" +
+				" consumer='cons'/>";
+		Document doc = XMLUtil.parse(uri);
+		CreateOrUpdateEntitiesParser parser = new CreateOrUpdateEntitiesParser();
+		Statement statement = parser.parse(doc.getDocumentElement(), new ResourceManagerSupport());
+		BeneratorContext context = new BeneratorContext();
+		ConsumerMock consumer = new ConsumerMock();
+		context.set("cons", consumer);
+		context.set("c", 100);
+		context.set("tc", 10);
+		context.set("ps", 20);
+		statement.execute(context);
+		assertEquals(100, consumer.invocationCount.get());
+	}
+
+	@Test
+	public void testSubCreate() throws Exception {
+        String consumerClassName = ConsumerMock.class.getName();
+		String uri = "string://" +
+        		"<create-entities type='top' count='3' consumer='cons1'>" +
+        		"    <create-entities type='sub' count='2' consumer='new " + consumerClassName + "(2)'/>" +
+        		"</create-entities>";
+		Document doc = XMLUtil.parse(uri);
+		CreateOrUpdateEntitiesParser parser = new CreateOrUpdateEntitiesParser();
+		Statement statement = parser.parse(doc.getDocumentElement(), new ResourceManagerSupport());
+		BeneratorContext context = new BeneratorContext();
+		ConsumerMock consumer1 = new ConsumerMock(1);
+		context.set("cons1", consumer1);
+		statement.execute(context);
+		assertEquals(3, consumer1.invocationCount.get());
+		assertFalse(consumer1.closed);
+		ConsumerMock consumer2 = ConsumerMock.instances.get(2);
+		assertEquals(2, consumer2.invocationCount.get());
+		assertTrue(consumer2.closed);
+	}
+
+	@Test
+	public void testSubCreateLoop() throws Exception {
+        // TODO test if sub loop that retrieves its loop length from the availability of a source, is properly reset between top loops
+	}
+
+}
