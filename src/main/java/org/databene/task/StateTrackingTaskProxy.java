8f36159d959965d1ddfc800c4d43fc4795068131
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-02-10 15:58:12 +0000

    Initial version of StateTrackingTaskProxy
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@3316 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/main/java/org/databene/task/StateTrackingTaskProxy.java
@@ -0,0 +1,66 @@
+/*
+ * (c) Copyright 2010 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.task;
+
+import org.databene.commons.BeanUtil;
+import org.databene.commons.Context;
+import org.databene.commons.ErrorHandler;
+
+/**
+ * Task proxy that remembers the result of the last execution step and provides it as 
+ * property <code>available</code>.<br/><br/>
+ * Created: 05.02.2010 10:41:55
+ * @since 0.6
+ * @author Volker Bergmann
+ */
+public class StateTrackingTaskProxy<E extends Task> extends TaskProxy<E> implements Cloneable {
+
+	protected boolean available;
+	
+	public StateTrackingTaskProxy(E realTask) {
+	    super(realTask);
+	    this.available = true;
+    }
+
+	public boolean isAvailable() {
+		return available;
+	}
+	
+	@Override
+	public synchronized boolean executeStep(Context context, ErrorHandler errorHandler) {
+		if (!available)
+			return false;
+	    available = super.executeStep(context, errorHandler);
+	    return available;
+	}
+	
+    @Override
+	public Object clone() {
+	    return new StateTrackingTaskProxy<E>(BeanUtil.clone(realTask));
+	}
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + '[' + realTask.toString() + ']';
+    }
+    
+}
