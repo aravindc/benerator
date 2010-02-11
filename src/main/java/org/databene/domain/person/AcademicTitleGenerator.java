5ada243e086a523f897cc71fed62f5cd8b1a5f42
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-02-11 12:35:48 +0000

    Javadoc
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@3383 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/domain/person/AcademicTitleGenerator.java
+++ src/main/java/org/databene/domain/person/AcademicTitleGenerator.java
@@ -1,3 +1,23 @@
+/*
+ * (c) Copyright 2006-2010 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.databene.domain.person;
 
 import org.databene.benerator.csv.LocalCSVGenerator;
@@ -6,8 +26,10 @@ import org.databene.commons.Encodings;
 import java.util.Locale;
 
 /**
- * (c) Copyright 2006 by Volker Bergmann
+ * Creates a quota of academic titles.<br/><br/>
  * Created: 26.06.2006 19:06:23
+ * @since 0.1
+ * @author Volker Bergmann
  */
 public class AcademicTitleGenerator extends LocalCSVGenerator<String> {
 
@@ -18,4 +40,5 @@ public class AcademicTitleGenerator extends LocalCSVGenerator<String> {
     public AcademicTitleGenerator(Locale locale) {
         super("org/databene/domain/person/title", locale, ".csv", Encodings.UTF_8);
     }
+    
 }
