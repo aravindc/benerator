77545e0fa17508bc01415fa30198e3a5f31fad22
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-02-12 11:39:38 +0000

    Testing RepeatSequence
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@3419 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/test/java/org/databene/benerator/distribution/sequence/RepeatSequenceTest.java
@@ -0,0 +1,58 @@
+/*
+ * (c) Copyright 2010 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.distribution.sequence;
+
+import java.math.BigDecimal;
+
+import org.databene.benerator.Generator;
+import org.databene.benerator.SequenceTestGenerator;
+import org.databene.benerator.test.GeneratorTest;
+import org.junit.Test;
+
+/**
+ * Tests the {@link RepeatSequence}.<br/><br/>
+ * Created: 12.02.2010 12:28:43
+ * @since 0.6.0
+ * @author Volker Bergmann
+ */
+public class RepeatSequenceTest extends GeneratorTest {
+
+	@Test
+	public void testCreateSequence() {
+		RepeatSequence sequence = createSequence122333();
+		Generator<Integer> generator = sequence.createGenerator(Integer.class, 1, 3, 1, false);
+		expectGeneratedSequence(generator, 1, 2, 2, 3, 3, 3).withCeasedAvailability();
+	}
+
+	private RepeatSequence createSequence122333() {
+	    return new RepeatSequence(0, 3, 1, new StepSequence(BigDecimal.ONE, BigDecimal.ZERO));
+    }
+	
+	@Test
+	public void testApplyTo() {
+        Generator<Integer> source = new SequenceTestGenerator<Integer>(1, 2, 3);
+		RepeatSequence sequence = createSequence122333();
+		Generator<Integer> generator = sequence.applyTo(source, false);
+		expectGeneratedSequence(generator, 1, 2, 2, 3, 3, 3).withCeasedAvailability();
+	}
+	
+}
