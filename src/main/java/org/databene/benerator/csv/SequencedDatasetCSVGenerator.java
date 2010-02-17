c2ef124e06eb37f383af96e2d4e3ad579a6f87c5
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-02-17 23:13:08 +0000

    Initial version of SequencedDatasetCSVGenerator
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@3519 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/main/java/org/databene/benerator/csv/SequencedDatasetCSVGenerator.java
@@ -0,0 +1,55 @@
+/*
+ * (c) Copyright 2010 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.csv;
+
+import java.util.List;
+
+import org.databene.benerator.distribution.Distribution;
+import org.databene.benerator.distribution.Sequence;
+import org.databene.benerator.sample.SampleGeneratorUtil;
+import org.databene.benerator.sample.SequencedSampleGenerator;
+import org.databene.benerator.sample.WeightedSample;
+import org.databene.benerator.wrapper.GeneratorProxy;
+import org.databene.commons.Converter;
+
+/**
+ * Generates values from a dataset based on a {@link Sequence}.<br/><br/>
+ * Created: 17.02.2010 23:22:52
+ * @since 0.6.0
+ * @author Volker Bergmann
+ */
+public class SequencedDatasetCSVGenerator<E> extends GeneratorProxy<E> {
+	
+	@SuppressWarnings("unchecked")
+    public SequencedDatasetCSVGenerator(String filenamePattern, char separator, String datasetName, String nesting,
+            Distribution distribution, String encoding, Converter<String, E> converter) {
+        List<E> samples = parseFiles(datasetName, separator, nesting, filenamePattern, encoding, converter);
+		setSource(new SequencedSampleGenerator<E>((Class<E>) samples.get(0).getClass(), distribution, samples));
+    }
+
+	private List<E> parseFiles(String datasetName, char separator, String nesting, String filenamePattern,
+            String encoding, Converter<String, E> converter) {
+        List<WeightedSample<E>> weightedSamples = CSVGeneratorUtil.parseDatasetFiles(datasetName, separator, nesting, filenamePattern, encoding, converter);
+        return SampleGeneratorUtil.extractValues(weightedSamples);
+    }
+
+}
