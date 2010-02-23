b3a297248f090bc443af890d26c44e08a9abbe13
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-02-23 11:23:43 +0000

    Introduced and tested DescriptorBasedGenerator
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@3655 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/test/java/org/databene/benerator/engine/statement/DescriptorBasedGeneratorTest.java
@@ -0,0 +1,65 @@
+/*
+ * (c) Copyright 2010 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.engine.statement;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.databene.benerator.Generator;
+import org.databene.benerator.engine.BeneratorContext;
+import org.databene.benerator.engine.DescriptorBasedGenerator;
+import org.databene.benerator.test.GeneratorTest;
+import org.databene.commons.SystemInfo;
+import org.databene.model.data.Entity;
+import org.junit.Test;
+
+/**
+ * Tests the {@link DescriptorBasedGenerator}.<br/><br/>
+ * Created: 23.02.2010 12:17:27
+ * @since TODO version
+ * @author Volker Bergmann
+ */
+public class DescriptorBasedGeneratorTest extends GeneratorTest {
+
+	@Test
+	public void testGetGenerator() throws Exception {
+		String lf = SystemInfo.getLineSeparator();
+		String uri = "string://<setup>" + lf +
+				"	<create-entities name='perGen' type='Person' count='1'>" + lf +
+				"		<attribute name='name' constant='Alice'/>" + lf +
+				"	</create-entities>" + lf +
+				"</setup>";
+		Generator<?> generator = new DescriptorBasedGenerator(uri, "perGen", new BeneratorContext());
+		assertEquals(Entity.class, generator.getGeneratedType());
+		assertNotNull(generator);
+		for (int i = 0; i < 10; i++)
+			checkGeneration(generator);
+		assertAvailable(generator);
+	}
+
+	private void checkGeneration(Generator<?> generator) {
+	    Entity entity = (Entity) generator.generate();
+		assertEquals("Person", entity.type());
+		assertEquals("Alice", entity.get("name"));
+    }
+	
+}
