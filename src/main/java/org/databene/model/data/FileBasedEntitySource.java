682cb9cf20ffd73d8b97398a5142cdeeba1b978c
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-03-12 10:24:06 +0000

    Parent class for file based EntitySources that is able to resolve relative URLs
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@3733 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/main/java/org/databene/model/data/FileBasedEntitySource.java
@@ -0,0 +1,68 @@
+/*
+ * (c) Copyright 2010 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.model.data;
+
+import org.databene.benerator.engine.BeneratorContext;
+import org.databene.commons.Context;
+import org.databene.commons.context.ContextAware;
+
+/**
+ * Parent class for {@link EntitySource}s that import entities from files.<br/><br/>
+ * Created: 12.03.2010 11:05:13
+ * @since 0.6.0
+ * @author Volker Bergmann
+ */
+public abstract class FileBasedEntitySource extends AbstractEntitySource implements ContextAware {
+	
+	protected String uri;
+	private BeneratorContext context;
+	
+	public FileBasedEntitySource(String uri) {
+	    this.uri = uri;
+    }
+
+    // properties ------------------------------------------------------------------------------------------------------
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+    
+    public void setContext(Context context) {
+    	this.context = (BeneratorContext) context;
+    }
+
+    protected String getAbsoluteUri() {
+    	return context.resolveRelativeUri(uri);
+    }
+    
+    // java.lang.Object overrides --------------------------------------------------------------------------------------
+
+    @Override
+	public String toString() {
+        return getClass().getSimpleName() + "[" + uri + "]";
+    }
+
+}
