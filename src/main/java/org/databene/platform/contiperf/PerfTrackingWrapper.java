4f3e267be13b48bba57b85a24f19f17971601a6d
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-03-14 11:57:47 +0000

    Common parent class for Benerator runners that support performance tracking
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@3766 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/main/java/org/databene/platform/contiperf/PerfTrackingWrapper.java
@@ -0,0 +1,102 @@
+/*
+ * (c) Copyright 2010 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.platform.contiperf;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+import org.databene.commons.ArrayBuilder;
+import org.databene.commons.ParseException;
+import org.databene.contiperf.ExecutionLogger;
+import org.databene.contiperf.Invoker;
+import org.databene.contiperf.PercentileRequirement;
+import org.databene.contiperf.PerformanceRequirement;
+import org.databene.contiperf.PerformanceTracker;
+import org.databene.contiperf.log.FileExecutionLogger;
+
+import freemarker.template.utility.StringUtil;
+
+/**
+ * Common parent class for Benerator runners that support performance tracking.<br/><br/>
+ * Created: 14.03.2010 10:59:00
+ * @since 0.6.0
+ * @author Volker Bergmann
+ */
+public abstract class PerfTrackingWrapper implements Closeable {
+
+	private PerformanceTracker tracker;
+	private PerformanceRequirement requirement;
+
+	protected abstract Invoker getInvoker();
+
+	public PerfTrackingWrapper() {
+	    this(null); // allow for lazy PerfromanceTracker initialization
+    }
+	
+	public PerfTrackingWrapper(PerformanceTracker tracker) {
+	    this.tracker = tracker;
+	    this.requirement = new PerformanceRequirement();
+    }
+	
+	public void setMax(int max) {
+		requirement.setMax(max);
+	}
+
+	public void setPercentiles(String percentilesSpec) {
+		requirement.setPercentiles(parsePercentilesSpec(percentilesSpec));
+	}
+	
+	public PerformanceTracker getTracker() {
+		if (tracker == null) {
+			// the tracker is initialized lazily for allowing the class to be first constructed in a simple way 
+			// and then be configured by calling the property setters.
+			Invoker invoker = getInvoker();
+			ExecutionLogger logger = new FileExecutionLogger();
+			tracker = new PerformanceTracker(invoker, requirement, logger);
+		}
+		return tracker;
+	}
+
+	@SuppressWarnings("unused")
+    public void close() throws IOException {
+	    tracker.stop();
+    }
+	
+	private PercentileRequirement[] parsePercentilesSpec(String percentilesSpec) {
+		String[] assignments = StringUtil.split(percentilesSpec, ',');
+		ArrayBuilder<PercentileRequirement> builder = new ArrayBuilder<PercentileRequirement>(
+				PercentileRequirement.class, assignments.length);
+		for (String assignment : assignments)
+			builder.add(parsePercentileSpec(assignment));
+	    return builder.toArray();
+    }
+
+	private PercentileRequirement parsePercentileSpec(String assignment) {
+	    String[] parts = StringUtil.split(assignment, ':');
+	    if (parts.length != 2)
+	    	throw new ParseException("Ilegal percentile syntax", assignment);
+	    int base  = Integer.parseInt(parts[0]);
+	    int limit = Integer.parseInt(parts[1]);
+		return new PercentileRequirement(base, limit);
+    }
+
+}
