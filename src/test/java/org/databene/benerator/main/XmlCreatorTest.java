8e732ca161f05a2023bf71754225b5b458917a7a
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-04-06 06:39:28 +0000

    Improved test
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@4014 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/test/java/org/databene/benerator/main/XmlCreatorTest.java
+++ src/test/java/org/databene/benerator/main/XmlCreatorTest.java
@@ -54,10 +54,12 @@ public class XmlCreatorTest {
 	
 	@Test
 	public void testEnum() throws IOException {
-        Document doc = createXMLFile(ENUM_FILE, "address", "target/enum.xml");
-        Element address = doc.getDocumentElement();
-        String box = address.getAttribute("box");
-        assertTrue("".equals(box) || "0203".equals(box));
+		for (int i = 0; i < 10; i++) {
+	        Document doc = createXMLFile(ENUM_FILE, "address", "target/enum.xml");
+	        Element address = doc.getDocumentElement();
+	        String box = address.getAttribute("box");
+	        assertTrue("".equals(box) || "0203".equals(box));
+		}
     }
 
     private Document createXMLFile(String schemaUri, String root, String filename) throws IOException {
