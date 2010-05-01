90b97999e690cafd5051968d471528d41aab3a10
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-05-01 06:14:46 +0000

    Descoped task
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@4126 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/task/runner/MultiThreadedTaskRunner.java
+++ src/main/java/org/databene/task/runner/MultiThreadedTaskRunner.java
@@ -100,7 +100,7 @@ public class MultiThreadedTaskRunner implements TaskRunner {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        if (threadSafe) // TODO v0.6.1 call pageFinished only if it was actually run in shared mode
+        if (threadSafe) // TODO v0.7 call pageFinished only if it was actually run in shared mode
             target.pageFinished();
         return counter.get();
     }
