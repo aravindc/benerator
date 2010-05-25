461bb29493bac3ccd2940b51f124c484051eda6d
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-05-25 14:00:15 +0000

    Bug fix 3006874 PersonGenerator.femaleQuota is broken
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@4287 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/domain/person/PersonGenerator.java
+++ src/main/java/org/databene/domain/person/PersonGenerator.java
@@ -51,6 +51,7 @@ public class PersonGenerator extends CompositeGenerator<Person> {
 	
 	private String datasetName;
 	private Locale locale;
+	private double femaleQuota;
 	
     private GenderGenerator genderGen;
     private GivenNameGenerator maleGivenNameGen;
@@ -85,11 +86,11 @@ public class PersonGenerator extends CompositeGenerator<Person> {
     // properties ------------------------------------------------------------------------------------------------------
 
 	public double getFemaleQuota() {
-		return genderGen.getFemaleQuota();
+		return femaleQuota;
 	}
 	
 	public void setFemaleQuota(double femaleQuota) {
-		genderGen.setFemaleQuota(femaleQuota);
+		this.femaleQuota = femaleQuota;
 	}
 	
 	public double getNobleQuota() {
@@ -125,7 +126,7 @@ public class PersonGenerator extends CompositeGenerator<Person> {
     public synchronized void init(GeneratorContext context) {
 		secondNameTest = registerComponent(new BooleanGenerator(0.2));
 		secondNameTest.init(context);
-		genderGen = registerComponent(new GenderGenerator());
+		genderGen = registerComponent(new GenderGenerator(femaleQuota));
 		genderGen.init(context);
         birthDateGenerator = registerComponent(new BirthDateGenerator(15, 105));
         birthDateGenerator.init(context);
