a7a1a5017785c481de1898f5ffee5309bfa132dd
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-08-01 20:34:41 +0000

    Added task
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@4554 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/platform/java/BeanDescriptorProvider.java
+++ src/main/java/org/databene/platform/java/BeanDescriptorProvider.java
@@ -84,6 +84,7 @@ public class BeanDescriptorProvider implements DescriptorProvider {
                 "char", Character.class,
             
                 "date", java.util.Date.class,
+                // TODO "duration" type?
                 "timestamp", java.sql.Timestamp.class,
 
                 "string", String.class,
