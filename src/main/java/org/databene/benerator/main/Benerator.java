b1bd0b419a9ef57bc334872ee2f4eea3c6834a17
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-08-01 20:28:37 +0000

    Supporting --version option
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@4547 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/benerator/main/Benerator.java
+++ src/main/java/org/databene/benerator/main/Benerator.java
@@ -28,16 +28,18 @@ package org.databene.benerator.main;
 
 import org.databene.benerator.Version;
 import org.databene.benerator.engine.DescriptorRunner;
+import org.databene.commons.ArrayUtil;
 import org.databene.commons.LogCategories;
 import org.databene.commons.SystemInfo;
 import org.databene.commons.VMInfo;
+import org.databene.commons.log.LoggingInfoPrinter;
 import org.databene.commons.ui.ConsoleInfoPrinter;
+import org.databene.commons.ui.InfoPrinter;
 import org.databene.model.version.VersionNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.*;
 
 import javax.script.ScriptEngineFactory;
 import javax.script.ScriptEngineManager;
@@ -53,47 +55,57 @@ public class Benerator {
 	public static final String LOCALE_VM_PARAM = "benerator.locale";
 	
 	private static final Logger logger = LoggerFactory.getLogger(Benerator.class);
-	private static final Logger configLogger = LoggerFactory.getLogger(LogCategories.CONFIG);
 
 	// methods ---------------------------------------------------------------------------------------------------------
 
 	public static void main(String[] args) throws IOException {
-		String filename = (args.length > 0 ? args[0] : "benerator.xml");
-		configLogger.debug("Running benerator " + Version.VERSION + " with file " + filename);
-		checkSystem();
-        listScriptEngines();
-		new DescriptorRunner(filename).run();
+		if (ArrayUtil.contains(args, "--version")) {
+			// print version info and exit
+			InfoPrinter console = new ConsoleInfoPrinter();
+			printVersionInfo(console);
+			System.exit(0);
+		} else {
+			// p
+			InfoPrinter printer = new LoggingInfoPrinter(LogCategories.CONFIG);
+			String filename = (args.length > 0 ? args[0] : "benerator.xml");
+			printer.printLines("Running file " + filename);
+			checkSystem(printer);
+			new DescriptorRunner(filename).run();
+		}
 	}
 
-	private static void listScriptEngines() {
-		// check installed JSR 223 script engines
-        ScriptEngineManager mgr = new ScriptEngineManager();
-        List<ScriptEngineFactory> engineFactories = mgr.getEngineFactories();
-        if (engineFactories.size() > 0) {
-        	configLogger.debug("Installed JSR 223 Script Engines:");
-			for (ScriptEngineFactory engineFactory : engineFactories) {
-				configLogger.debug("- " + engineFactory.getEngineName() + engineFactory.getNames());
-	        }
-        }
-	}
-
-	private static void checkSystem() {
-		configLogger.debug("Max heap size: " + Runtime.getRuntime().maxMemory() / 1024 / 1024 + " MB");
-		configLogger.debug("Java " + VMInfo.getJavaVersion());
-		configLogger.debug(VMInfo.getJavaVmName() + " " + VMInfo.getJavaVmVersion() + " (" + VMInfo.getJavaVmVendor() + ")");
-		configLogger.debug(SystemInfo.getOsName() + " " + SystemInfo.getOsVersion() + " (" + SystemInfo.getOsArchitecture() + ")");
+	private static void checkSystem(InfoPrinter printer) {
+		printVersionInfo(printer);
+		printer.printLines("Max heap size: " + Runtime.getRuntime().maxMemory() / 1024 / 1024 + " MB");
 		try {
 			Class.forName("javax.script.ScriptEngine");
 		} catch (ClassNotFoundException e) {
-			ConsoleInfoPrinter.printHelp("You need to run benerator with Java 6 or greater!");
+			System.out.println("You need to run benerator with Java 6 or greater!");
 			if (SystemInfo.isMacOsx())
-				ConsoleInfoPrinter.printHelp("Please check the reference manual for Java setup on Mac OS X.");
+				System.out.println("Please check the manual for Java setup on Mac OS X.");
 			System.exit(-1);
 		}
 		VersionNumber javaVersion = new VersionNumber(VMInfo.getJavaVersion());
 		if (javaVersion.compareTo(new VersionNumber("1.6")) < 0)
 			logger.warn("benerator is written for and tested under Java 6 - " +
 					"you managed to set up JSR 226, but may face other problems.");
+	}
+
+	private static void printVersionInfo(InfoPrinter printer) {
+		printer.printLines(
+			"Benerator " + Version.VERSION,
+			"Java version " + VMInfo.getJavaVersion(),
+			"JVM " + VMInfo.getJavaVmName() + " " + VMInfo.getJavaVmVersion() + " (" + VMInfo.getJavaVmVendor() + ")",
+			"OS " + SystemInfo.getOsName() + " " + SystemInfo.getOsVersion() + " (" + SystemInfo.getOsArchitecture() + ")"
+		);
+        listScriptEngines(printer);
+    }
+
+	private static void listScriptEngines(InfoPrinter printer) {
+    	printer.printLines("Installed JSR 223 Script Engines:");
+        for (ScriptEngineFactory engine : new ScriptEngineManager().getEngineFactories()) {
+			printer.printLines("- " + engine.getEngineName() + engine.getNames());
+        }
 	}
 
 }
