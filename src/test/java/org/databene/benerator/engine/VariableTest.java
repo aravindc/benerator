f3154d691727059217e6e222b2fbeb1453625777
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-08-19 15:26:29 +0000

    Testing bug #3048752: 'this' not available in <iterate>
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@4657 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/test/java/org/databene/benerator/engine/VariableTest.java
@@ -0,0 +1,68 @@
+/*
+ * (c) Copyright 2010 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.engine;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.databene.benerator.test.ConsumerMock;
+import org.databene.benerator.test.PersonIterable;
+import org.databene.model.data.Entity;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * TODO Document class.<br/><br/>
+ * Created: 10.08.2010 07:06:46
+ * @since 0.6.4
+ * @author Volker Bergmann
+ */
+public class VariableTest extends BeneratorIntegrationTest {
+
+	private ConsumerMock<Entity> consumer;
+	
+    @Before
+	public void setUp() throws Exception {
+		consumer = new ConsumerMock<Entity>(true);
+		context.set("cons", consumer);
+		context.set("pit", new PersonIterable());
+	}
+	
+	// test methods ----------------------------------------------------------------------------------------------------
+
+	@Test
+	public void testIterateThis() {
+		parseAndExecute(
+				"<iterate type='referer' source='pit' consumer='cons'>" +
+				"	<variable name='_n' script='this.name' converter='org.databene.commons.converter.ToUpperCaseConverter' />" +
+				"	<attribute name='name' script='_n' />" +
+				"</iterate>");
+		List<Entity> products = consumer.getProducts();
+		assertEquals(2, products.size());
+		assertEquals("ALICE", products.get(0).get("name"));
+		assertEquals(23,      products.get(0).get("age"));
+		assertEquals("BOB",   products.get(1).get("name"));
+		assertEquals(34,      products.get(1).get("age"));
+	}
+
+}
