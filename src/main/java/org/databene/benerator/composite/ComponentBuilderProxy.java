16fb463ffa40e205f6f280beee692f8e8e5664c6
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-10-17 10:24:19 +0000

    Introduced ConditionalComponentBuilder and ComponentBuilderProxy
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@4775 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/main/java/org/databene/benerator/composite/ComponentBuilderProxy.java
@@ -0,0 +1,70 @@
+/*
+ * (c) Copyright 2010 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.composite;
+
+import org.databene.benerator.GeneratorContext;
+
+/**
+ * Proxy class for a {@link ComponentBuilder}.<br/><br/>
+ * Created: 11.10.2010 11:10:51
+ * @since 0.6.4
+ * @author Volker Bergmann
+ */
+public class ComponentBuilderProxy<E> implements ComponentBuilder<E> {
+	
+	protected ComponentBuilder<E> source;
+	protected GeneratorContext context;
+
+	public ComponentBuilderProxy(ComponentBuilder<E> source) {
+	    this.source = source;
+    }
+
+	public Class<?> getGeneratedType() {
+	    return source.getGeneratedType();
+    }
+
+	public boolean isParallelizable() {
+	    return source.isParallelizable();
+    }
+
+	public boolean isThreadSafe() {
+	    return source.isThreadSafe();
+    }
+
+	public void init(GeneratorContext context) {
+		this.context = context;
+	    source.init(context);
+    }
+
+	public boolean buildComponentFor(E target) {
+	    return source.buildComponentFor(target);
+    }
+
+	public void reset() {
+	    source.reset();
+    }
+
+	public void close() {
+	    source.close();
+    }
+
+}
