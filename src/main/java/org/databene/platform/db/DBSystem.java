7c46b9e3fbe9224e7b945bd53008c746ebef4bb7
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2010-12-08 22:32:51 +0000

    Adopted name change
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@4875 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/platform/db/DBSystem.java
+++ src/main/java/org/databene/platform/db/DBSystem.java
@@ -620,7 +620,7 @@ public class DBSystem extends AbstractStorageSystem {
             String[] foreignKeyColumnNames = constraint.getForeignKeyColumnNames();
             if (foreignKeyColumnNames.length == 1) {
                 String fkColumnName = foreignKeyColumnNames[0];
-                DBTable targetTable = constraint.getForeignTable();
+                DBTable targetTable = constraint.getRefereeTable();
                 DBColumn fkColumn = constraint.getTable().getColumn(fkColumnName);
                 DBColumnType concreteType = fkColumn.getType();
                 String abstractType = JdbcMetaTypeMapper.abstractType(concreteType, acceptUnknownColumnTypes);
