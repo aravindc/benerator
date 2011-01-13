facf4980dc0797793ce7532f13f7e6e8850dcb9e
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2011-01-13 15:14:32 +0000

    Testing transcoding feature
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@4989 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/test/java/org/databene/benerator/engine/TranscodingIntegrationTest.java
@@ -0,0 +1,79 @@
+/*
+ * (c) Copyright 2011 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.engine;
+
+import static org.junit.Assert.*;
+
+import java.io.Closeable;
+import java.util.Iterator;
+
+import org.databene.commons.TypedIterable;
+import org.databene.model.data.Entity;
+import org.databene.platform.db.DBSystem;
+import org.junit.Test;
+
+/**
+ * Integration test for Benerator's transcoding feature.<br/><br/>
+ * Created: 12.01.2011 17:06:25
+ * @since 0.6.4
+ * @author Volker Bergmann
+ */
+public class TranscodingIntegrationTest {
+
+	private static final String DESCRIPTOR_FILE_NAME = "src/test/resources/org/databene/benerator/engine/transcode/TranscodingIntegrationTest.ben.xml";
+	
+	@Test
+	public void testSimpleCase() throws Exception {
+		// run descriptor file
+		DescriptorRunner runner = new DescriptorRunner(DESCRIPTOR_FILE_NAME);
+		runner.run();
+		BeneratorContext context = runner.getContext();
+		DBSystem t = (DBSystem) context.get("t");
+		// check countries
+		TypedIterable<Entity> iterable = t.queryEntities("country", null, context);
+		Iterator<Entity> iterator = iterable.iterator();
+		assertNextCountry(1, "United States", iterator);
+		assertNextCountry(2, "Germany", iterator);
+		assertFalse(iterator.hasNext());
+		((Closeable) iterator).close();
+		// check states
+		iterable = t.queryEntities("state", null, context);
+		iterator = iterable.iterator();
+		assertNextState(1, 1, "California", iterator);
+		assertNextState(2, 1, "Florida", iterator);
+		assertNextState(3, 2, "Bayern", iterator);
+		assertNextState(4, 2, "Hamburg", iterator);
+		assertFalse(iterator.hasNext());
+		((Closeable) iterator).close();
+	}
+
+	private void assertNextCountry(int id, String name, Iterator<Entity> iterator) {
+		assertTrue(iterator.hasNext());
+		assertEquals(new Entity("COUNTRY", "ID", id, "NAME", name), iterator.next());
+	}
+	
+	private void assertNextState(int id, int countryId, String name, Iterator<Entity> iterator) {
+		assertTrue(iterator.hasNext());
+		assertEquals(new Entity("STATE", "ID", id, "COUNTRY_FK", countryId, "NAME", name), iterator.next());
+	}
+	
+}
---
+++ src/test/resources/org/databene/benerator/engine/transcode/createCountryAndStateTables.sql
@@ -0,0 +1,13 @@
+create table COUNTRY (
+  ID     int         not null,
+  NAME   varchar(30) not null,
+  constraint COUNTRY_PK primary key (ID)
+);
+
+create table STATE (
+  ID               int         not null,
+  COUNTRY_FK       int         not null,
+  NAME             varchar(30) not null,
+  constraint STATE_PK primary key (ID),
+  constraint STATE_COUNTRY_FK foreign key (COUNTRY_FK) references COUNTRY (ID)
+);
