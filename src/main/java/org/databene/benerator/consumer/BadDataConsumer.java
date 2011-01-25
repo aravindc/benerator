3bd232427925cb697a0de3cce7adb96a2f262676
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2011-01-25 15:50:09 +0000

    Defined BadDataConsumer
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@5066 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/main/java/org/databene/benerator/consumer/BadDataConsumer.java
@@ -0,0 +1,64 @@
+/*
+ * (c) Copyright 2011 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.consumer;
+
+import org.databene.model.consumer.Consumer;
+import org.databene.model.consumer.ConsumerProxy;
+
+/**
+ * TODO Document class.<br/><br/>
+ * Created: 23.01.2011 08:04:17
+ * @since TODO version
+ * @author Volker Bergmann
+ */
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class BadDataConsumer extends ConsumerProxy {
+	
+	Consumer badDataTarget;
+	
+	public BadDataConsumer(Consumer badDataTarget, Consumer realTarget) {
+		super(realTarget);
+		this.badDataTarget = badDataTarget;
+	}
+
+	public void startConsuming(Object object) {
+		try {
+			target.startConsuming(object);
+		} catch (Exception e) {
+			badDataTarget.startConsuming(object);
+			badDataTarget.finishConsuming(object);
+		}
+	}
+
+	@Override
+	public void flush() {
+		super.flush();
+		badDataTarget.flush();
+	}
+	
+	@Override
+	public void close() {
+		super.close();
+		badDataTarget.close();
+	}
+	
+}
