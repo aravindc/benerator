a7ca0533081a794430e9ef925aa808ff2f23b906
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2011-03-24 10:40:34 +0000

    Improved test setup
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@5255 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/test/java/org/databene/domain/person/BirthDateGeneratorTest.java
+++ src/test/java/org/databene/domain/person/BirthDateGeneratorTest.java
@@ -23,14 +23,14 @@ public class BirthDateGeneratorTest extends GeneratorClassTest {
 
     @Test
     public void test() throws IllegalGeneratorStateException {
-        Date now = new Date();
-        BirthDateGenerator generator = new BirthDateGenerator(3, 80);
+        Date now = TimeUtil.today();
+        BirthDateGenerator generator = new BirthDateGenerator(3, 12);
         generator.init(context);
         for (int i = 0; i < 1000; i++) {
-            Date birtDate = generator.generate();
-            int age = TimeUtil.yearsBetween(birtDate, now);
-            assertTrue("Generated birthdate is to new: " + birtDate, age >= 3);
-            assertTrue("Generated birthdate is to old: " + birtDate, age <= 80);
+            Date birthDate = generator.generate();
+            int age = TimeUtil.yearsBetween(birthDate, now);
+            assertTrue("Generated birth date is too new: " + birthDate, age >= 3);
+            assertTrue("Generated birth date is too old: " + birthDate, age <= 12);
         }
     }
     
