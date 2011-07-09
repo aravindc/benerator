becfba2b7bbb87d31a2b850acc1b3a868622fd95
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2011-07-09 09:35:46 +0000

    Defined Generator wrappers that add a null value to the set of generated products
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@5593 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/main/java/org/databene/benerator/nullable/NullStartingNullableGenerator.java
@@ -0,0 +1,58 @@
+/*
+ * (c) Copyright 2011 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.nullable;
+
+import org.databene.benerator.GeneratorContext;
+import org.databene.benerator.InvalidGeneratorSetupException;
+import org.databene.benerator.wrapper.ProductWrapper;
+
+/**
+ * {@link NullableGenerator} implementation which wraps a source {@link NullableGenerator}
+ * but generates a null value before forwarding the products of the source.<br/><br/>
+ * Created: 08.07.2011 17:49:26
+ * @since 0.7.0
+ * @author Volker Bergmann
+ */
+public class NullStartingNullableGenerator<E> extends NullableGeneratorProxy<E> {
+	
+	private boolean nullUsed;
+	
+	public NullStartingNullableGenerator(NullableGenerator<E> source) {
+		super(source);
+		this.nullUsed = false;
+	}
+	
+	@Override
+	public void init(GeneratorContext context) throws InvalidGeneratorSetupException {
+		super.init(context);
+	}
+	
+	@Override
+	public ProductWrapper<E> generate(ProductWrapper<E> wrapper) {
+		if (!nullUsed) {
+			this.nullUsed = true;
+			return wrapper.setProduct(null);
+		}
+		return super.generate(wrapper);
+	}
+	
+}
