6dd2beecea7c6d564df690279efea37b087400ec
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2011-07-20 18:03:02 +0000

    Improved error messaging
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@5666 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/benerator/script/BeneratorScriptParser.java
+++ src/main/java/org/databene/benerator/script/BeneratorScriptParser.java
@@ -267,7 +267,7 @@ public class BeneratorScriptParser {
 			if (stop.getStopIndex() == 0)
 				throw new SyntaxError("Syntax error after " + stop.getText(), text);
 			else
-				throw new SyntaxError("Syntax error at the beginning ", text);
+				throw new SyntaxError("Syntax error", text);
 		}
 	}
 	
