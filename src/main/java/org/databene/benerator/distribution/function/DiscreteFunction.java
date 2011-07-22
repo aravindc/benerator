545a07d2805d1c0a6d5890fca4917be3c00bf7d7
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2011-07-22 07:10:21 +0000

    Task completed
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@5700 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/benerator/distribution/function/DiscreteFunction.java
+++ src/main/java/org/databene/benerator/distribution/function/DiscreteFunction.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2006-2009 by Volker Bergmann. All rights reserved.
+ * (c) Copyright 2006-2011 by Volker Bergmann. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, is permitted under the terms of the
@@ -40,7 +40,7 @@ public class DiscreteFunction extends AbstractWeightFunction {
 
     private double[] values;
 
-    public DiscreteFunction(double ... weights) { // TODO v0.6.x support varargs arguments in BeneratorScript
+    public DiscreteFunction(double ... weights) {
         this.values = weights;
     }
 
