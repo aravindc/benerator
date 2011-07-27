a2bd16ec964ac2713ffedd4fbc527bdca63012f9
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2011-07-27 08:19:10 +0000

    adopted interface change
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@5787 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/benerator/wrapper/SynchronizedGeneratorProxy.java
+++ src/main/java/org/databene/benerator/wrapper/SynchronizedGeneratorProxy.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2010 by Volker Bergmann. All rights reserved.
+ * (c) Copyright 2010-2011 by Volker Bergmann. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, is permitted under the terms of the
@@ -50,8 +50,8 @@ public class SynchronizedGeneratorProxy<E> implements Generator<E> {
 	    return source.getGeneratedType();
     }
 
-	public synchronized E generate() {
-	    return source.generate();
+	public synchronized ProductWrapper<E> generate(ProductWrapper<E> wrapper) {
+	    return source.generate(wrapper);
     }
 
 	public synchronized void reset() {
