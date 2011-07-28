bdc8d59f89e15d73a1a86500436efafbd2cb228d
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2011-07-28 11:15:02 +0000

    Avoiding name collision
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@5900 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/test/java/org/databene/benerator/engine/parser/xml/RunTaskParserAndStatementTest.java
+++ src/test/java/org/databene/benerator/engine/parser/xml/RunTaskParserAndStatementTest.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2009-2010 by Volker Bergmann. All rights reserved.
+ * (c) Copyright 2009-2011 by Volker Bergmann. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, is permitted under the terms of the
@@ -40,7 +40,7 @@ import org.junit.Test;
 public class RunTaskParserAndStatementTest extends BeneratorIntegrationTest {
 
 	@Before
-	public void setUp() {
+	public void setUpTaskMock() {
 		TaskMock.count.set(0);
 	}
 	
