033b733a80211c2f327a36f670a441ee969a7bb0
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2012-03-05 07:07:04 +0000

    bug fix in tracker management
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@6660 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/platform/contiperf/PerfTrackingWrapper.java
+++ src/main/java/org/databene/platform/contiperf/PerfTrackingWrapper.java
@@ -77,7 +77,8 @@ public abstract class PerfTrackingWrapper implements Closeable {
 	}
 
     public void close() {
-	    tracker.stop();
+    	if (tracker.isStarted())
+    		tracker.stop();
     }
 
 }
