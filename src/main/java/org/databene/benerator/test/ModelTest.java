6d5a071df49e60044926f2e19ea7ed316e906da7
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2012-12-20 10:59:37 +0000

    Adopted API change
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@6868 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/benerator/test/ModelTest.java
+++ src/main/java/org/databene/benerator/test/ModelTest.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2011 by Volker Bergmann. All rights reserved.
+ * (c) Copyright 2011-2012 by Volker Bergmann. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, is permitted under the terms of the
@@ -21,8 +21,8 @@
 
 package org.databene.benerator.test;
 
+import org.databene.benerator.BeneratorFactory;
 import org.databene.benerator.engine.BeneratorContext;
-import org.databene.benerator.engine.DefaultBeneratorContext;
 import org.databene.model.data.ArrayElementDescriptor;
 import org.databene.model.data.ArrayTypeDescriptor;
 import org.databene.model.data.ComplexTypeDescriptor;
@@ -55,7 +55,7 @@ public abstract class ModelTest {
 
     @Before
     public void setUpContextAndDescriptorProvider() throws Exception {
-    	this.context = new DefaultBeneratorContext();
+    	this.context = BeneratorFactory.getInstance().createContext(".");
         this.context.importDefaults();
         this.dataModel = context.getDataModel();
         this.testDescriptorProvider = new DefaultDescriptorProvider("test", context.getDataModel());
