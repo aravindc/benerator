f6c251b36ad605dc85345a2e89f0f0bb0686907b
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2013-01-25 17:53:23 +0000

    Supporting 'format' flag in imports
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@6938 37d91da1-6759-5a44-b2bd-541c34f01409

--- src/main/java/org/databene/benerator/engine/DescriptorConstants.java
+++ src/main/java/org/databene/benerator/engine/DescriptorConstants.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2009-2011 by Volker Bergmann. All rights reserved.
+ * (c) Copyright 2009-2013 by Volker Bergmann. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, is permitted under the terms of the
@@ -100,6 +100,7 @@ public interface DescriptorConstants {
 	static final String ATT_MESSAGE = "message";
 	static final String ATT_SELECTOR = "selector";
 	static final String ATT_SOURCE = "source";
+	static final String ATT_FORMAT = "format";
 	static final String ATT_OFFSET = "offset";
     static final String ATT_REF = "ref";
     static final String ATT_VALUE = "value";
