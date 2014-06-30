88ade53154abcfc1e94e39015b53d7c3a96d960f
vbergmann <vbergmann@37d91da1-6759-5a44-b2bd-541c34f01409>
2014-06-30 12:45:29 +0000

    Testing TemplateFileEntityExporter and EDI features
    git-svn-id: https://svn.code.sf.net/p/benerator/code/benerator/trunk@7401 37d91da1-6759-5a44-b2bd-541c34f01409

---
+++ src/test/java/org/databene/benerator/engine/TemplateFileEntityExporterIntegrationTest.java
@@ -0,0 +1,70 @@
+/*
+ * (c) Copyright 2014 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.benerator.engine;
+
+import org.databene.benerator.Consumer;
+import org.databene.benerator.test.BeneratorIntegrationTest;
+import org.databene.commons.IOUtil;
+import org.databene.platform.template.TemplateFileEntityExporter;
+import org.junit.Test;
+
+/**
+ * Tests the {@link TemplateFileEntityExporter}.<br/><br/>
+ * Created: 27.06.2014 17:42:34
+ * @since 0.9.7
+ * @author Volker Bergmann
+ */
+
+public class TemplateFileEntityExporterIntegrationTest extends BeneratorIntegrationTest {
+
+	@Test
+	public void testCountries() throws Exception {
+		String uri = "target/" + getClass().getName() + ".txt";
+		String templateUri = "org/databene/benerator/engine/template/countries.ftl";
+		BeneratorContext context = parseAndExecute(
+			"<setup>" +
+			"<bean id='con' class='TemplateFileEntityExporter'>" + 
+			"	<property name='uri' value='" + uri + "'/>" + 
+			"	<property name='templateUri' value='" + templateUri + "'/>" + 
+			"</bean>" +
+			"<generate type='countries' count='3' consumer='con'>" +
+	    	"	<attribute name='name' pattern='[A-Z]{5,10}' />" + 
+	    	"	<attribute name='population' type='int' min='1000000' max='100000000' />" + 
+			"   <generate type='states' minCount='3' maxCount='7' consumer='con'>" +
+	    	"      <attribute name='name' pattern='[A-Z]{5,10}' />" + 
+	    	"   </generate>" +
+	    	"</generate>" +
+	    	"</setup>");
+		Consumer con = (Consumer) context.get("con");
+		con.close();
+		String content = IOUtil.getContentOfURI(uri);
+		System.out.println(content);
+	}
+
+	@Test
+	public void testIFTDGN() throws Exception {
+		parseAndExecuteFile("org/databene/benerator/engine/template/IFTDGN.ben.xml");
+		String content = IOUtil.getContentOfURI("target/IFTDGN.edi");
+		System.out.println(content);
+	}
+
+}
---
+++ src/test/resources/org/databene/benerator/engine/template/IFTDGN.ftl
@@ -0,0 +1,34 @@
+<#list iftdgns as iftdgn>
+UNB+UNOA:2+CAPSTAN+HSDG+130821:1259+OPH'
+UNH+${iftdgn.message_ref_no_E_0062_1_01}+IFTDGN:D:03A:UN:PROT20'
+BGM+89N+${iftdgn.message_function_code_E_1225_2_03!}+${iftdgn.response_type_code_E_4343_2_04}'
+HAN+L'
+TDT+20+${iftdgn.vessel_voy_dir_vn_E_8028_10_02}${iftdgn.vessel_voy_dir_d_E_8028_10_02}+1+${iftdgn.vessel_id_C_C001_10_04}++++${iftdgn.transport_id_C_C222_10_08}'
+RFF+${iftdgn.ref_C_C506_12_01}'
+LOC+153+${iftdgn.pol_ot_C_C517_14_02}:139:6'
+DTM+132:130822:101'
+DTM+133:130823:101'
+LOC+61+${iftdgn.npoc_ot_C_C517_14_02}:139:6'
+EQD+CN+${iftdgn.equBbkRorItem_ot_C_C237_22_02}+${iftdgn.equipment_size_and_type_C_C224_22_03}'
+MEA+WT+AAN+TNE:26'
+<#list iftdgn.consignmentInformations_L_CNI_25 as cni>
+CNI+${cni_index+1}+${cni.bookingNo_ot_C_C503_25_02}'
+HAN+L'
+LOC+9+${cni.pol_ot_C_C517_28_02}:139:6'
+LOC+11+${cni.pod_ot_C_C517_28_02}:139:6'
+<#list cni.goodsItemDetails_L_GID_42 as gid>
+GID+${gid_index+1}+${gid.no_and_type_of_packages_C_C213_42_02}'
+DGS+IMD+3+1263:24C+24:CEL+3+F-E'
+FTX+AAD+++PAINT'
+MEA+WT+AAL+KGM:21113'
+MEA+WT+G+KGM:0'
+RFF+APN:${gid.dgrNo_ot_C_C506_53_01}'
+SGP+${iftdgn.equBbkRorItem_ot_C_C237_22_02}'
+LOC+147+${gid.stowagePosition_ot_C_C517_56_02}'
+MEA+WT+AAL+KGM:21113'
+MEA+WT+G+KGM:0'
+</#list>
+</#list>
+UNT+${iftdgn.recursiveSegmentCount}+${iftdgn.message_ref_no_E_0062_1_01}'
+UNZ+1+OPH'
+</#list>
